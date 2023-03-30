452daa804bb8b7830027372c8db2df1a432e94a5
Roy Kishony <rkishony@technion.ac.il>
2023-03-31 00:36:31 +0300

    add Conversation module with tests

---
+++ icd_analysis/tests/functional/conversation.py
@@ -0,0 +1,24 @@
+from _pytest.fixtures import fixture
+
+from scientistgtp import Conversation, Role
+
+
+@fixture()
+def conversation():
+    conversation = Conversation()
+    conversation.append_message(Role.SYSTEM, 'You are a helpful assistant.')
+    conversation.append_message(Role.USER, 'How are you?')
+    return conversation
+
+
+def test_conversation_gpt_response(conversation):
+    assert len(conversation) == 2, "sanity"
+    response = conversation.get_response(should_print=False, should_append=True)
+    assert len(response)
+    assert len(conversation) == 3
+
+
+def test_conversation_gpt_response_without_appending(conversation):
+    response = conversation.get_response(should_print=False, should_append=False)
+    assert len(response)
+    assert len(conversation) == 2
--- scientistgtp/conversation.py
+++ scientistgtp/conversation.py
@@ -1,23 +1,33 @@
+from enum import Enum
+from .env import OPENAI_API_KEY, MODEL_ENGINE
+
 import openai
 
-from typing import Dict, List
-
 # Set up the OpenAI API client
-openai.api_key = "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"
+openai.api_key = OPENAI_API_KEY
 
-# Set up the model and prompt
-model_engine = "gpt-3.5-turbo"
+
+class Role(str, Enum):
+    SYSTEM = 'system'
+    USER = 'user'
+    ASSISTANT = 'assistant'
 
 
 class Conversation(list):
+    """
+    a list of message exchange between user and chatgtp.
 
+    a Conversation instance allows:
+    1. appending user queries.
+    2. getting and appending gpt response.
+    """
     @staticmethod
     def print_message(message: str, should_print: bool = True):
         if should_print:
             print('-----------------')
             print('\n' + message + '\n')
 
-    def append_message(self, role: str, message: str, should_print: bool = False):
+    def append_message(self, role: Role, message: str, should_print: bool = False):
         self.append({'role': role, 'content': message})
         self.print_message(message, should_print)
 
@@ -25,11 +35,11 @@ class Conversation(list):
                      should_print: bool = True,
                      should_append: bool = True) -> str:
         response = openai.ChatCompletion.create(
-            model=model_engine,
+            model=MODEL_ENGINE,
             messages=self,
         )
         response_message = response['choices'][0]['message']['content']
         if should_append:
-            self.append_message('assistant', response_message)
+            self.append_message(Role.ASSISTANT, response_message)
         self.print_message(response_message, should_print)
         return response_message
