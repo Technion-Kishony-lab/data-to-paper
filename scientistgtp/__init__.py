317d7f241f7cfa16075f144702fbdc69b4508314
Roy Kishony <rkishony@technion.ac.il>
2023-03-31 12:16:51 +0300

    init

--- scientistgtp/__init__.py
+++ scientistgtp/__init__.py
@@ -1,3 +1,4 @@
-from conversation import Conversation
-from run_code import RunCode, RunCoodeException, FailedRunningCode, FailedExtractingCode, FailedLoadingOutput
-from scientistgtp import ScientistGTP
+from .conversation import Conversation, Role
+from .code_runner import CodeRunner, FailedRunningCode, FailedExtractingCode, FailedLoadingOutput
+from .scientist_gpt import ScientistGTP
+
