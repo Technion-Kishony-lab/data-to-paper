20359f1c718777a10503886646bbed5380c1493e
Roy Kishony <rkishony@technion.ac.il>
2023-03-31 15:01:06 +0300

    first semi-functional run of icd code analysis

---
+++ icd_analysis/create_icd_analysis_paper.py
@@ -0,0 +1,9 @@
+from queries import data_description, goal_description
+
+from scientistgtp import ScientistGTP, ANALYSIS_PLAN
+
+runner = ScientistGTP(run_plan=ANALYSIS_PLAN,
+                      data_description=data_description,
+                      goal_description=goal_description)
+
+runner.run_all()
