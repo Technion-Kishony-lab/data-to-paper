32a7d2c05265c2560c0629bfe6531cff3df183e1
Roy Kishony <rkishony@technion.ac.il>
2023-03-31 12:26:16 +0300

    all tests work

--- scientistgtp/scientist_gpt.py
+++ scientistgtp/scientist_gpt.py
@@ -1,9 +1,9 @@
-from dataclasses import dataclass
-from typing import Optional
+import copy
+from typing import Optional, List, Callable, Any, Tuple, NamedTuple
 
-from conversation import Conversation
-from run_code import RunCode, RunCoodeException
-
+from .conversation import Conversation, Role
+from .code_runner import CodeRunner
+from .exceptions import RunCodeException
 
 OUTPUT_FILENAME = 'results.txt'
 
@@ -17,41 +17,39 @@ class ScientistGTP:
 
     def initialize_conversation(self):
         self.conversation = Conversation()
-        self.conversation.append_message('system', 'You are a helpful scientist.')
+        self.conversation.append_message(Role.SYSTEM, 'You are a helpful scientist.')
 
     def add_data_description(self):
-        self.conversation.append_message('user', 'We have the following data files:\n\n' + self.data_description)
+        self.conversation.append_message(Role.USER, 'We have the following data files:\n\n' + self.data_description)
 
     def add_goal_description(self):
-        self.conversation.append_message('user', self.goal_description)
+        self.conversation.append_message(Role.USER, self.goal_description)
 
     def request_analysis_plan(self):
         self.initialize_conversation()
         self.add_data_description()
         self.add_goal_description()
-        self.conversation.append_message('user', 'Suggest a data analysis plan to achieve the specified goal.')
+        self.conversation.append_message(Role.USER, 'Suggest a data analysis plan to achieve the specified goal.')
         self.data_analysis_plan = self.conversation.get_response()
         return self.data_analysis_plan
 
     def request_analysis_code(self):
-        self.conversation.append_message('user',
+        self.conversation.append_message(Role.USER,
                                          f'Write a complete Python code to perform the analysis you suggested.\n'
                                          f'The output of the code should be a text file named `{OUTPUT_FILENAME}`.')
 
     def run_analysis_code(self):
-        result = None
-        counter = 4
-        while counter > 0 and result is None:
-            counter -= 1
-            analysis_code_response = self.conversation.get_response()
-            try:
-                result = RunCode(response=analysis_code_response, output_file=OUTPUT_FILENAME)
-            except RunCoodeException:
-                pass
-
-        if result is None:
-            raise RuntimeError()
+        self.request_analysis_code()
+        analysis_code_response = self.conversation.get_response(should_append=False)
+        result = CodeRunner(response=analysis_code_response, output_file=OUTPUT_FILENAME)
+        self.conversation.append_message(Role.ASSISTANT, analysis_code_response)
 
     def analyze_and_write_paper(self):
         self.request_analysis_plan()
 
+
+'''
+request analysis plan
+request and run code
+
+'''
