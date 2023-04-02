997aa0e4a74626b34e212d0cc19d018e8b0c9bad
TalIfargan <talifargan@gmail.com>
2023-04-02 20:42:26 +0300

    added ReviewerGPT which revise the analysis plan, made progress for automatically creating a paper (WIP)

--- scientistgpt/env.py
+++ scientistgpt/env.py
@@ -1,4 +1,4 @@
-SUPPORTED_PACKAGES = ['numpy', 'pandas', 'scipy']
+SUPPORTED_PACKAGES = ['numpy', 'pandas', 'scipy', 'matplotlib', 'seaborn', 'sklearn']
 
 OPENAI_API_KEY = "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"
 
--- scientistgpt/gpt_interactors/scientist_gpt.py
+++ scientistgpt/gpt_interactors/scientist_gpt.py
@@ -5,12 +5,14 @@ from scientistgpt.proceed_retract import FuncAndRetractions, RunPlan
 from scientistgpt.exceptions import FailedDebuggingException
 from scientistgpt.conversation import Conversation
 from scientistgpt.utils.text_utils import format_str, print_red
+from scientistgpt.env import SUPPORTED_PACKAGES
+from .reviewer_gpt import ReviewerGPT
 
 from .debugger_gpt import DebuggerGPT
 from .converser_gpt import ConverserGPT
 
 GPT_SCRIPT_FILENAME = 'gpt_analysis'
-
+MAX_ANALYSIS_REVISIONS = 2
 
 class ScientistGPT(ConverserGPT):
     """
@@ -36,10 +38,12 @@ class ScientistGPT(ConverserGPT):
                  conversation: Optional[Conversation] = None,
                  data_description: Optional[str] = None,
                  goal_description: Optional[str] = None,
+                 analysis_plan: Optional[str] = None,
                  ):
         super().__init__(run_plan, conversation)
         self.data_description = data_description
         self.goal_description = goal_description
+        self.analysis_plan = analysis_plan
         self._run_code_attempt = 0
 
     def add_data_description(self):
@@ -61,14 +65,27 @@ class ScientistGPT(ConverserGPT):
             Suggest a data analysis plan to achieve the specified goal.
             """)
         self.conversation.append_user_message(prompt)
-        return self.conversation.get_response_from_chatgpt()
+        reply = self.conversation.get_response_from_chatgpt()
+        self.analysis_plan = self.conversation.get_last_response()
+        return reply
 
     def request_analysis_code(self):
         prompt = format_str(f"""
             Write a complete Python code to perform the analysis you suggested.
+            Use only the packages: {SUPPORTED_PACKAGES} to perform the analysis.
             The output of the code should be a text file named `{self.OUTPUT_FILENAME}`.
+            The plots should be saved as image files and not displayed to screen.
             """)
         self.conversation.append_user_message(prompt)
+
+    def review_analysis_plan(self):
+        print_red('Ask ReviewerGPT to find flaws within the plan  ...')
+        reviewer = ReviewerGPT(conversation=copy.deepcopy(self.conversation))
+        result = reviewer.review_plan(self.analysis_plan)
+        self.analysis_plan = result
+        # replace the last message with the updated plan
+        self.conversation.delete_last_response()
+        self.conversation.append_assistant_message(self.analysis_plan)
 
     def run_gpt_code_and_add_output_to_conversation(self):
         print_red('Transfer control to DebuggerGPT to debug and get a functional code ...')
@@ -85,15 +102,40 @@ class ScientistGPT(ConverserGPT):
             ```
             {}
             ```
-
-            Do these results make sense, or do you suspect any problems, bugs or artifacts in the analysis code?
-
-            If you suspect a problem, please provide a new full compete code which correctly resolves the problem.
+            
+            You are obliged to choose one of the following options:
+            1. I am satisfied with the analysis and the results, I am ready to write a paper about them.
+            2. I need to write additional code the code and try again before writing a paper.
+            
+            Answer with the number of the option you choose only, e.g. "1".
             """).format(self.OUTPUT_FILENAME, result)
         self.conversation.append_user_message(prompt)
 
     def get_gpt_response_to_analysis(self):
-        return self.conversation.get_response_from_chatgpt()
+        analysis_revises = 0
+        while analysis_revises < MAX_ANALYSIS_REVISIONS:
+            result = self.conversation.get_response_from_chatgpt(should_append=False)
+            if result == '1':
+                break
+
+            if result == '2':
+                prompt = format_str("""
+                Write additional code to improve the analysis and try again. write any additional results to the output file.
+                """)
+                self.conversation.append_user_message(prompt)
+                self.conversation.get_response_from_chatgpt()
+                self.run_gpt_code_and_add_output_to_conversation()
+            analysis_revises += 1
+
+def write_paper(self):
+    # TODO: take data description, goal description, analysis plan and code and results and create a paper
+    prompt = format_str("""
+    Write the paper, write abstract, introduction, methods, results, discussion and acknowledgments.
+    In addition you are required to state where to add the figure of you created during the analysis by using
+    FIGURE@#@ name_of_figure @#@ where name_of_figure is the name of the figure you want to enter.
+    """)
+    self.conversation.append_user_message(prompt)
+    self.conversation.get_response_from_chatgpt()
 
 
 upon_code_failure = [1, 1, 2, 1, 1]
@@ -108,7 +150,9 @@ ScientistGPT_ANALYSIS_PLAN: RunPlan = [
     FuncAndRetractions('add_data_description', (), []),
     FuncAndRetractions('add_goal_description', (), []),
     FuncAndRetractions('request_analysis_plan', (), []),
+    FuncAndRetractions('review_analysis_plan', (), []),
     FuncAndRetractions('request_analysis_code', (), []),
     FuncAndRetractions('run_gpt_code_and_add_output_to_conversation', FailedDebuggingException, upon_code_failure),
     FuncAndRetractions('get_gpt_response_to_analysis', (), []),
+
 ]
