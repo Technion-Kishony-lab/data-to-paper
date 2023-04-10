7a9bbbe206321368226076ddb079c9e075d53ded
Roy Kishony <rkishony@technion.ac.il>
2023-04-10 18:19:30 +0300

    env adjustments (can undo)

--- scientistgpt/env.py
+++ scientistgpt/env.py
@@ -1,4 +1,5 @@
-SUPPORTED_PACKAGES = ['numpy', 'pandas', 'scipy', 'matplotlib', 'seaborn', 'sklearn']
+#SUPPORTED_PACKAGES = ['numpy', 'pandas', 'scipy', 'matplotlib', 'seaborn', 'sklearn']
+SUPPORTED_PACKAGES = ['numpy', 'pandas', 'scipy', 'matplotlib']
 
 OPENAI_API_KEY = "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"
 
@@ -7,4 +8,4 @@ MODEL_ENGINE = "gpt-3.5-turbo"
 TEXT_WIDTH = 120
 
 # max time for code timeout when running code from chatgpt (seconds)
-MAX_EXEC_TIME = 900
+MAX_EXEC_TIME = 20
