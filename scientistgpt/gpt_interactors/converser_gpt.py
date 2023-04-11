398fcf558ffe6f5a1656a409a70f0ad58606f71b
Roy Kishony <rkishony@technion.ac.il>
2023-04-11 09:13:22 +0300

    clean and organize dialog conversers

--- scientistgpt/gpt_interactors/converser_gpt.py
+++ scientistgpt/gpt_interactors/converser_gpt.py
@@ -1,5 +1,7 @@
 from dataclasses import dataclass
+from typing import Optional
 
+from scientistgpt import Role
 from scientistgpt.conversation.converation_manager import ConversationManager
 
 
@@ -29,9 +31,11 @@ class ConverserGPT:
     def conversation(self):
         return self.conversation_manager.conversation
 
-    def initialize_conversation(self):
-        self.conversation_manager.create_conversation()
-        self.conversation_manager.append_system_message(self._system_prompt)
+    def initialize_conversation_if_needed(self):
+        if self.conversation_manager.conversation is None:
+            self.conversation_manager.create_conversation()
+        if len(self.conversation) == 0:
+            self.conversation_manager.append_system_message(self._system_prompt)
 
 
 @dataclass
@@ -59,80 +63,131 @@ class DialogConverserGPT(ConverserGPT):
     def other_conversation(self):
         return self.other_conversation_manager.conversation
 
-    def initialize_other_conversation(self):
-        self.other_conversation_manager.create_conversation()
-        self.other_conversation_manager.append_system_message(self._other_system_prompt)
+    def initialize_other_conversation_if_needed(self):
+        if self.other_conversation_manager.conversation is None:
+            self.other_conversation_manager.create_conversation()
+        if len(self.other_conversation) == 0:
+            self.other_conversation_manager.append_system_message(self._other_system_prompt)
 
 
 @dataclass
 class RoleReversalDialogConverserGPT(DialogConverserGPT):
     """
-    A base class for agents running a dialog between two chatgpts, where the roles of the two agents are reversed.
+    A base class for agents running a dialog between two chatgpts (self and other), where the roles of the two
+    agents are reversed. The ASSISTANT response from one conversation is used as the USER response in the other
+    conversation, and vice versa.
+
+    A conversation is formed between the two agents. The conversation is terminated when the other agent
+    terminates the conversation by issuing a termination_phrase, or when a maximum number of rounds has been reached.
+
+    The conversation can start with self either after its first response or before.
     """
 
-    def get_next_response(self):
-        """
-        Get the next response from the other agent.
-        """
-        response = self.conversation_manager.get_and_append_assistant_message()
-        self.other_conversation_manager.append_user_message(self._alter_response(response))
+    #                                               end if
+    #                         can start        exceeds max_rounds
+    #                           here                  ^
+    #                            |                    |
+    #                            v               self_response
+    #      end if             SELF.chatgpt  -------------------->  OTHER.user
+    # termination_phrase   <---  |                                     |
+    # in other_response       SELF.user     <--------------------  OTHER.chatgpt
+    #                           ^                other_response
+    #                           |                round_num += 1
+    #                         or, can
+    #                         start here
 
-    def get_next_other_response(self):
+    termination_phrase: str = 'Job completed'
+    """
+    A phrase used by the 'other' chatgpt to terminate the conversation.
+    """
+
+    max_rounds: int = 5
+
+    def __post_init__(self):
+        super().__post_init__()
+        self.round_num = 0
+
+    def get_response_from_other_in_response_to_response_from_self(self, self_response: str) -> str:
         """
-        Get the next response from the other agent.
+        Append response from self as user message to other conversation, and get response from other assistant.
         """
-        other_response = self.other_conversation_manager.get_and_append_assistant_message()
+        self.round_num += 1
+        self.other_conversation_manager.append_user_message(self._alter_self_response(self_response))
+        return self.other_conversation_manager.get_and_append_assistant_message()
+
+    def get_response_from_self_in_response_to_response_from_other(self, other_response: str) -> str:
+        """
+        Append response from other as user message to self conversation, and get response from assistant.
+        """
         self.conversation_manager.append_user_message(self._alter_other_response(other_response))
+        return self.conversation_manager.get_and_append_assistant_message()
 
-    def _alter_response(self, response: str) -> str:
+    def _alter_self_response(self, response: str) -> str:
         """
-        Alter the response from the agent.
+        Alter the response from self.
         """
         return response
 
     def _alter_other_response(self, response: str) -> str:
         """
-        Alter the response from the other agent.
+        Alter the response from other.
         """
         return response
 
-    def run_one_round_starting_with_self(self):
+    def is_completed(self) -> bool:
         """
-        Run one round of the dialog.
+        The dialog is completed when the other agent terminates the conversation, by responding with the
+        termination phrase.
         """
-        self.get_next_response()
-        self.get_next_other_response()
+        return len(self.other_conversation_manager.conversation) > 1 and \
+            self.termination_phrase.lower() in self.other_conversation_manager.conversation.get_last_response().lower()
 
-    def run_one_round_starting_with_other(self):
+    def run_dialog(self, append_termination_response_to_self: bool = True) -> str:
         """
-        Run one round of the dialog.
+        Run the dialog until it is completed.
+        Returns the last chatgpt response from self.
         """
-        self.get_next_other_response()
-        self.get_next_response()
+        while True:
+            response = self.run_one_cycle(append_termination_response_to_self)
+            if response is not None:
+                return response
+
+    def run_one_cycle(self, append_termination_response_to_self: bool = True) -> Optional[str]:
+        """
+        Run one cycle of the dialog. Return str of response if completed, or None if not completed
+        """
+
+        # to allow starting either before or after the first self response:
+        if self.conversation[-1].role is Role.USER:
+            self_response = self.conversation_manager.get_and_append_assistant_message()
+        else:
+            self_response = self.conversation.get_last_response()
+
+        if self.round_num >= self.max_rounds:
+            return self_response
+
+        other_response = self.get_response_from_other_in_response_to_response_from_self(self_response)
+
+        if self.is_completed():
+            if append_termination_response_to_self:
+                self.conversation_manager.append_user_message(other_response)
+            return self_response
+
+        self.get_response_from_self_in_response_to_response_from_other(other_response)
 
 
 @dataclass
 class ConstructiveReviewDialogConverserGPT(RoleReversalDialogConverserGPT):
     """
-    A base class for agents running a dialog between two chatgpts one is a reviwee who needs to perform a task,
-    and the other is a reviewer who provides constructive feedback.
+    A base class for agents running a dialog between two chatgpts, where one is a "reviwee" who needs to perform a task
+    towards a certain "goal", and the other is a "reviewer" who provides constructive feedback.
+
+    The interaction proceeds in repeated cycles of the reviwee performing the task and the reviewer providing feedback.
     """
 
+    # Properties that should be set by each instance according to the task we want to perform:
     reviewee: str = 'scientist'
     reviewer: str = 'scientific reviewer'
-
-    system_prompt: str = \
-        'You are a {reviewee} who needs to {goal_verb} {goal_noun}.\n' \
-        'I will be your {reviewer}.'
-
-    other_system_prompt: str = \
-        'You are a {reviewer} for a {reviewee} who needs to {goal_verb} {goal_noun}.\n' \
-        'Your job is to advise me, the {reviewee}, and provide constructive bullet-point feedback in repeated cycles ' \
-        'of improvements and feedback.\n' \
-        'When you feel that the goal has been achieved and you cannot advise of additional improvements, then ' \
-        'respond with just two words: "Job completed".\n' \
-        '\n' \
-        'I will be the {reviewee} that you advise.'
 
     goal_noun: str = 'a one-paragraph summary on the solar system'
     """
@@ -145,48 +200,53 @@ class ConstructiveReviewDialogConverserGPT(RoleReversalDialogConverserGPT):
     The verb applied to the goal noun, like 'write', 'draw', 'build', 'code', etc.
     """
 
-    max_rounds: int = 5
+    # Properties that are more generic (can adjust if needed):
+    system_prompt: str = \
+        'You are a {reviewee} who needs to {goal_verb} {goal_noun}.\n' \
+        'I will be your {reviewer}.'
 
-    def _replace_system_prompt(self, system_prompt):
+    user_initiation_prompt: str = \
+        'Hello {reviewee}. Please {goal_verb} {goal_noun}.'
+
+    other_system_prompt: str = \
+        'You are a {reviewer} for a {reviewee} who needs to {goal_verb} {goal_noun}.\n' \
+        'Your job is to advise me, the {reviewee}, and provide constructive bullet-point feedback in repeated cycles ' \
+        'of improvements and feedback.\n' \
+        'When you feel that the goal has been achieved and you cannot advise of additional improvements, then ' \
+        'respond explicitly with: "{termination_phrase}".\n' \
+        '\n' \
+        'I will be the {reviewee} that you advise.'
+
+    sentence_to_add__at_the_end_of_reviewer_response: str = \
+        'Please correct your response and send back a complete rewrite of the {goal_noun}.'
+
+    def _format_prompt(self, system_prompt):
         return system_prompt.format(reviewee=self.reviewee, reviewer=self.reviewer,
+                                    termination_phrase=self.termination_phrase,
                                     goal_noun=self.goal_noun, goal_verb=self.goal_verb)
 
     @property
     def _system_prompt(self):
-        return self._replace_system_prompt(self.system_prompt)
+        return self._format_prompt(self.system_prompt)
 
     @property
     def _other_system_prompt(self):
-        return self._replace_system_prompt(self.other_system_prompt)
+        return self._format_prompt(self.other_system_prompt)
 
     def _alter_other_response(self, response: str) -> str:
-        """
-        Alter the response from the other agent.
-        """
-        return response + f'\n\nPlease correct your response and send back a complete rewrite of the {self.goal_noun}.'
+        return response + '\n\n' + self._format_prompt(self.sentence_to_add__at_the_end_of_reviewer_response)
 
     def _pre_populate_conversation(self):
-        self.conversation_manager.append_user_message(
-            f'Hello {self.reviewee}. Please {self.goal_verb} {self.goal_noun}.')
+        self.conversation_manager.append_user_message(self._format_prompt(self.user_initiation_prompt))
 
-    def initialize_dialog(self):
-        self.initialize_conversation()
-        self.initialize_other_conversation()
+    def initialize_dialog(self, suppress_printing_of_other: bool = True):
+        self.initialize_conversation_if_needed()
+        self.initialize_other_conversation_if_needed()
 
         # Disable printing of the other_conversation because one is the same of the other (except for role reversal)
-        self.other_conversation_manager.should_print = False
+        if suppress_printing_of_other:
+            self.other_conversation_manager.should_print = False
         self._pre_populate_conversation()
-
-    def is_completed(self):
-        return len(self.other_conversation_manager.conversation) > 1 and \
-            'job completed' in self.other_conversation_manager.conversation.get_last_response().lower()
-
-    def run_dialog(self) -> str:
-        for i in range(self.max_rounds):
-            self.run_one_round_starting_with_self()
-            if self.is_completed():
-                return self.conversation_manager.conversation[-2].content
-        raise RuntimeError('The conversation did not end with "Job completed"')
 
 
 @dataclass
