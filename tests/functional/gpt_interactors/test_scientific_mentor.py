455277ce1c158b467a53dea67fda9270966367b9
Roy Kishony <rkishony@technion.ac.il>
2023-04-12 22:28:24 +0300

    fix some imports. all tests pass

--- tests/functional/gpt_interactors/test_scientific_mentor.py
+++ tests/functional/gpt_interactors/test_scientific_mentor.py
@@ -1,6 +1,6 @@
 import pytest
 
-from scientistgpt import ScientificGPT, ScientistGPT_EXECUTION_PLAN
+from scientistgpt import ScientificGPT
 from tests.utils import record_or_replay_openai
 
 
@@ -8,7 +8,6 @@ from tests.utils import record_or_replay_openai
 @record_or_replay_openai
 def test_scientific_mentor():
     runner = ScientificGPT(
-        execution_plan=ScientistGPT_EXECUTION_PLAN,
         data_description='file named "data.csv" with columns "gender" (F/M) and "height" (in cm) representing '
                          'the height of a person.',
         goal_description='I am interested whether there is a significant height difference between females and males')
