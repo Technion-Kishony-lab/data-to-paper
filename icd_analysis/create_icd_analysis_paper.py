963c4b70615855c5d56017de157b07c7e5693afd
Roy Kishony <rkishony@technion.ac.il>
2023-04-12 22:54:02 +0300

    fix icd analysis demo

--- icd_analysis/create_icd_analysis_paper.py
+++ icd_analysis/create_icd_analysis_paper.py
@@ -5,13 +5,15 @@ from pathlib import Path
 from queries import data_description, goal_description
 
 from scientistgpt import ScientificGPT
+from scientistgpt.conversation.replay import save_actions_to_file
 from scientistgpt.run_gpt_code.dynamic_code import module_dir
+from tests.utils import record_or_replay_openai
 
 """
 Set folders
 """
 DATA_FOLDER = 'data_for_analysis'
-MESSAGES_FILENAME = 'openai_exchange.txt'
+ACTIONS_FILENAME = 'openai_exchange.txt'
 OUTPUTS_FOLDER = 'run3'
 
 # Get absolute paths:
@@ -33,7 +35,13 @@ os.chdir(absolute_data_path)
 
 runner = ScientificGPT(data_description=data_description, goal_description=goal_description)
 
-runner.run_all()
+
+@record_or_replay_openai
+def run_all():
+    runner.run_all()
+
+
+run_all()
 
 os.chdir(absolute_home_path)
 
@@ -41,7 +49,7 @@ os.chdir(absolute_home_path)
 Save results
 """
 # Save conversation to text file:
-runner.conversation.save(absolute_output_path / MESSAGES_FILENAME)
+save_actions_to_file(absolute_output_path / ACTIONS_FILENAME)
 
 # Move all gpt analysis result files to output folder:
 for file in glob.glob(str(absolute_data_path / (ScientificGPT.gpt_script_filename + '*.txt'))):
@@ -55,6 +63,7 @@ for file in glob.glob(str(Path(module_dir) / (ScientificGPT.gpt_script_filename 
 for file in glob.glob(str(absolute_data_path / '*.png')):
     shutil.move(file, absolute_output_path)
 
+# TODO: this one is risky cs it might move our own data files (if they have .txt extension)
 # Move gpt generated txt files to output folder:
 for file in glob.glob(str(absolute_data_path / '*.txt')):
     shutil.move(file, absolute_output_path)
