03a1e0122f7df038eb865a5ebd3000b70df7eb62
Roy Kishony <rkishony@technion.ac.il>
2023-04-13 14:16:42 +0300

    refactor mock openai

--- scientistgpt/__init__.py
+++ scientistgpt/__init__.py
@@ -1,4 +1,4 @@
 from .conversation import Conversation, Role, Message
+from .conversation.replay import replay_actions, load_actions_from_file, clear_actions_and_conversations
 from .gpt_interactors.scientist_gpt import ScientistGPT
 from .user_utils import run_scientist_gpt, view_saved_conversation
-
