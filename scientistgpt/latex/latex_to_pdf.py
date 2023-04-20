4bf3b763054eb61f7663bca62db5b243cb3458f7
Roy Kishony <rkishony@technion.ac.il>
2023-04-20 07:41:04 +0300

    linting

--- scientistgpt/latex/latex_to_pdf.py
+++ scientistgpt/latex/latex_to_pdf.py
@@ -3,9 +3,9 @@ import requests
 
 def latex_to_pdf(latex_content: str, filename: str):
     """
-    Convert a LaTeX file to PDF using latexonline.cc server and save the PDF to local file.
+    Convert a LaTeX file to PDF using latexonline.cc server and save the PDF as a local file.
     """
-    assert filename.endswith('.pdf'),  'The filename should end with .pdf'
+    assert filename.endswith('.pdf'), 'The filename should end with .pdf'
 
     url = 'https://latexonline.cc/compile'
     data = {
@@ -17,4 +17,3 @@ def latex_to_pdf(latex_content: str, filename: str):
 
     with open(filename, 'wb') as f:
         f.write(response.content)
-
