3e3434cf71242233d39c8046aa6b51ad9d9739cb
TalIfargan <talifargan@gmail.com>
2023-04-24 01:23:26 +0300

    compiling pdf with bibtex works (new test pass)

--- scientistgpt/latex/latex_to_pdf.py
+++ scientistgpt/latex/latex_to_pdf.py
@@ -1,26 +1,26 @@
 import os
-from typing import Optional
+import subprocess
 
 
-def compile_latex_file_to_pdf(latex_file_path: str, bib_file_name: Optional[str] = None):
+def compile_latex_file_to_pdf(latex_file_path: str, should_compile_with_bib: bool = False):
     latex_file_path = os.path.abspath(latex_file_path)
-    os.system(f'pdflatex -output-directory {os.path.dirname(latex_file_path)} {latex_file_path}')
-
-    if bib_file_name is not None:
-        os.system(f'bibtex {bib_file_name}')
-        # We need to run pdflatex twice to get the references right:
-        os.system(f'pdflatex -output-directory {os.path.dirname(latex_file_path)} {latex_file_path}')
-        os.system(f'pdflatex -output-directory {os.path.dirname(latex_file_path)} {latex_file_path}')
+    latex_file_name = os.path.basename(latex_file_path)
+    os.chdir(os.path.dirname(latex_file_path))
+    subprocess.run(['pdflatex', latex_file_name], check=True)
+    if should_compile_with_bib:
+        subprocess.run(['bibtex', latex_file_name.replace('.tex', '')], check=True)
+        subprocess.run(['pdflatex', latex_file_name], check=True)
+        subprocess.run(['pdflatex', latex_file_name], check=True)
 
     # pdflatex creates some additional files we don't need:
-    for ext in ['.aux', '.log', '.out']:
+    for ext in ['.aux', '.log', '.out', '.bbl', '.blg']:
         if os.path.exists(latex_file_path.replace('.tex', ext)):
             os.remove(latex_file_path.replace('.tex', ext))
 
 
-def save_latex_and_compile_to_pdf(latex_content: str, file_path: str, bib_file_name: Optional[str] = None,
+def save_latex_and_compile_to_pdf(latex_content: str, file_path: str, should_compile_with_bib: bool = False,
                                   should_compile_to_pdf: bool = True):
     with open(file_path + '.tex', 'w') as f:
         f.write(latex_content)
     if should_compile_to_pdf:
-        compile_latex_file_to_pdf(file_path + '.tex', bib_file_name)
+        compile_latex_file_to_pdf(file_path + '.tex', should_compile_with_bib)
--- tests/functional/latex/test_latex_to_pdf.py
+++ tests/functional/latex/test_latex_to_pdf.py
@@ -15,11 +15,68 @@ Hello World!
 '''
 
 
+@fixture()
+def latex_content_with_citations():
+    return r'''
+\documentclass{article}
+\usepackage{cite}
+\usepackage{hyperref}
+\begin{document}
+Hello World! \cite{cite1} \cite{cite2}
+\bibliographystyle{plain}
+\bibliography{citations}
+\end{document}
+'''
+
+
+def create_citations_file(tmpdir):
+    citations_file = tmpdir.join('citations.bib').strpath
+    with open(citations_file, 'w') as f:
+        f.write(r'''
+@article{cite1,
+    title={Citation 1},
+    author={Author 1},
+    journal={Journal 1},
+    year={2020},
+    volume={1},
+    number={1},
+    pages={1-1},
+    doi={10.1111/1111},
+    url={https://www.example.com/1}
+}
+
+@article{cite2,
+    title={Citation 2},
+    author={Author 2},
+    journal={Journal 2},
+    year={2020},
+    volume={2},
+    number={2},
+    pages={2-2},
+    doi={10.1111/2222},
+    url={https://www.example.com/2}
+}
+''')
+
+
 def test_latex_to_pdf(tmpdir, latex_content):
     file_path = tmpdir.join('test').strpath
     save_latex_and_compile_to_pdf(latex_content, file_path)
 
     assert os.path.exists(file_path + '.tex')
     assert os.path.exists(file_path + '.pdf')
+    assert not os.path.exists(file_path + '.aux')
+    assert not os.path.exists(file_path + '.log')
+
+
+def test_latex_to_pdf_with_bibtex(tmpdir, latex_content_with_citations):
+    file_path = tmpdir.join('test').strpath
+    create_citations_file(tmpdir)
+    save_latex_and_compile_to_pdf(latex_content_with_citations, file_path, should_compile_with_bib=True)
+
+    assert os.path.exists(file_path + '.tex')
+    assert os.path.exists(file_path + '.pdf')
+    assert not os.path.exists(file_path + '.blg')
+    assert not os.path.exists(file_path + '.bbl')
     assert not os.path.exists(file_path + '.aux')
     assert not os.path.exists(file_path + '.log')
