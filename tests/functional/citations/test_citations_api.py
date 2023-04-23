93abdcc41c96935c7f9a7be31c483f2399174c1c
TalIfargan <talifargan@gmail.com>
2023-04-24 02:14:03 +0300

    recording works!

--- tests/functional/citations/test_citations_api.py
+++ tests/functional/citations/test_citations_api.py
@@ -1,11 +1,10 @@
-from scientistgpt.gpt_interactors.citation_adding.call_crossref import CrossrefServerCaller, CROSSREF_SERVER_CALLER
+from scientistgpt.gpt_interactors.citation_adding.call_crossref import CROSSREF_SERVER_CALLER
 
 QUERY = 'covid-19 affected the world'
 
 
 @CROSSREF_SERVER_CALLER.record_or_replay()
 def test_get_crossref_response():
-    crossref_server_caller = CrossrefServerCaller()
-    citations = crossref_server_caller.get_server_response(QUERY)
+    citations = CROSSREF_SERVER_CALLER.get_server_response(QUERY)
 
     assert len(citations) > 0
