21ca74111fbae806085647af2aa26bedc6f55d46
Roy Kishony <rkishony@technion.ac.il>
2023-04-24 13:17:31 +0300

    latex compilation in temp dir

--- scientistgpt/latex/latex_to_pdf.py
+++ scientistgpt/latex/latex_to_pdf.py
@@ -1,26 +1,44 @@
 import os
+import shutil
+from contextlib import contextmanager
+from pathlib import Path
 import subprocess
 
 
-def compile_latex_file_to_pdf(latex_file_path: str, should_compile_with_bib: bool = False):
-    latex_file_path = os.path.abspath(latex_file_path)
-    latex_file_name = os.path.basename(latex_file_path)
-    os.chdir(os.path.dirname(latex_file_path))
-    subprocess.run(['pdflatex', latex_file_name], check=True)
-    if should_compile_with_bib:
-        subprocess.run(['bibtex', latex_file_name.replace('.tex', '')], check=True)
-        subprocess.run(['pdflatex', latex_file_name], check=True)
-        subprocess.run(['pdflatex', latex_file_name], check=True)
-
-    # pdflatex creates some additional files we don't need:
-    for ext in ['.aux', '.log', '.out', '.bbl', '.blg']:
-        if os.path.exists(latex_file_path.replace('.tex', ext)):
-            os.remove(latex_file_path.replace('.tex', ext))
+# Temp directory for latex complication:
+module_dir = os.path.dirname(__file__)
+TEMP_FOLDER_FOR_LATEX_COMPILE = (Path(module_dir) / 'temp_latex_compile').absolute()
 
 
-def save_latex_and_compile_to_pdf(latex_content: str, file_path: str, should_compile_with_bib: bool = False,
+@contextmanager
+def run_in_temp_directory():
+    cwd = os.getcwd()
+    if not os.path.exists(TEMP_FOLDER_FOR_LATEX_COMPILE):
+        os.mkdir(TEMP_FOLDER_FOR_LATEX_COMPILE)
+    os.chdir(TEMP_FOLDER_FOR_LATEX_COMPILE)
+    try:
+        yield
+    finally:
+        os.chdir(cwd)
+        shutil.rmtree(TEMP_FOLDER_FOR_LATEX_COMPILE)
+
+
+def save_latex_and_compile_to_pdf(latex_content: str, file_name: str, output_directory: str,
+                                  should_compile_with_bib: bool = False,
                                   should_compile_to_pdf: bool = True):
-    with open(file_path + '.tex', 'w') as f:
-        f.write(latex_content)
-    if should_compile_to_pdf:
-        compile_latex_file_to_pdf(file_path + '.tex', should_compile_with_bib)
+    latex_file_name = file_name + '.tex'
+    with run_in_temp_directory():
+        with open(latex_file_name, 'w') as f:
+            f.write(latex_content)
+        if should_compile_to_pdf:
+            subprocess.run(['pdflatex', latex_file_name], check=True)
+            if should_compile_with_bib:
+                subprocess.run(['bibtex', file_name], check=True)
+                subprocess.run(['pdflatex', latex_file_name], check=True)
+                subprocess.run(['pdflatex', latex_file_name], check=True)
+
+        # Move the pdf and the latex and the citation file to the original directory:
+        shutil.move(file_name + '.pdf', output_directory)
+        shutil.move(latex_file_name, output_directory)
+        if should_compile_with_bib:
+            shutil.move('citations.bib', output_directory)
