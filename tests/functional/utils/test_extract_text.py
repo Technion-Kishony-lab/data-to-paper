5bb0897d4699ce15e985db7eeaa39962b0d0721f
TalIfargan <talifargan@gmail.com>
2023-04-24 15:05:33 +0300

    update test_extract_text to show non-working example

--- tests/functional/utils/test_extract_text.py
+++ tests/functional/utils/test_extract_text.py
@@ -1,13 +1,21 @@
 from scientistgpt.utils import extract_text_between_tags
 
-text = 'hello, here is a list [1, 2, 3, [4], 5] of numbers and lists'
+text_1 = 'hello, here is a list [1, 2, 3, [4], 5] of numbers and lists'
+text_2 = '\\title{\\textbf{this is some title in bold}} \n' \
+         '\\start{abstract} this is also the abstract \\end{abstract}'
+text_3 = '\\section{Introduction} \n' \
+            'this is the introduction \n' \
 
 
 def test_extract_text_between_tags():
-    assert extract_text_between_tags(text, '[', ']') == '1, 2, 3, [4], 5'
-    assert extract_text_between_tags(text, '[', ']', leave_tags=True) == '[1, 2, 3, [4], 5]'
+    assert extract_text_between_tags(text_1, '[', ']') == '1, 2, 3, [4], 5'
+    assert extract_text_between_tags(text_1, '[', ']', leave_tags=True) == '[1, 2, 3, [4], 5]'
+    assert extract_text_between_tags(text_2, '\\title{', '}') == '\\textbf{this is some title in bold}'
+    assert extract_text_between_tags(text_2, '\\title{', '}', leave_tags=True) == '\\title{\\textbf{this is some title in bold}}'
+    assert extract_text_between_tags(text_2, '\\start{', '}') == 'abstract'
 
 
 def test_extract_text_between_tags_open_ended():
-    assert extract_text_between_tags(text, '[', None) == '1, 2, 3, [4], 5] of numbers and lists'
-    assert extract_text_between_tags(text, '[', None, leave_tags=True) == '[1, 2, 3, [4], 5] of numbers and lists'
+    assert extract_text_between_tags(text_1, '[', None) == '1, 2, 3, [4], 5] of numbers and lists'
+    assert extract_text_between_tags(text_1, '[', None, leave_tags=True) == '[1, 2, 3, [4], 5] of numbers and lists'
+    assert extract_text_between_tags(text_3, '\\section{Introduction}', None) == ' \nthis is the introduction \n' \
