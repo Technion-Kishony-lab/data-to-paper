d6ef02438435bbd142b827360979083924b1de25
Roy Kishony <rkishony@technion.ac.il>
2023-04-27 15:43:57 +0300

    hotfix: fix bug in extract_text

--- scientistgpt/utils/text_utils.py
+++ scientistgpt/utils/text_utils.py
@@ -113,7 +113,7 @@ def extract_text_between_tags(text: str, left_tag: str, right_tag: str = None, l
             start = text.find(left_tag)
             if start == -1:
                 raise ValueError(f'Could not find left tag {left_tag} in text')
-            end = text.find(right_tag, start)
+            end = text.find(right_tag, start + len(left_tag))
             if end == -1:
                 raise ValueError(f'Could not find left tag {right_tag} in text')
             if leave_tags:
@@ -134,13 +134,13 @@ def extract_text_between_tags(text: str, left_tag: str, right_tag: str = None, l
         return text[start + len(left_tag):]
 
 
-def extract_text_between_brackets(text: str, open_bracket: str):
+FROM_OPEN_BRACKET_TO_CLOSE_BRACKET = {'[': ']', '{': '}', '(': ')'}
+
+
+def extract_text_between_brackets(text: str, open_bracket: str, leave_brackets: bool = False):
     """
     use stack to find matching closing bracket for the first open bracket, use stack to find matching closing bracket.
     return the text between the first open bracket and the matching closing bracket without the brackets.
-    :param text:
-    :param open_bracket:
-    :return:
     """
     start = text.find(open_bracket)
     if start == -1:
@@ -150,13 +150,11 @@ def extract_text_between_brackets(text: str, open_bracket: str):
     while len(stack) > 0:
         if text[end] == open_bracket:
             stack.append(open_bracket)
-        elif text[end] == ']' and open_bracket == '[':
-            stack.pop()
-        elif text[end] == '}' and open_bracket == '{':
-            stack.pop()
-        elif text[end] == ')' and open_bracket == '(':
+        elif text[end] == FROM_OPEN_BRACKET_TO_CLOSE_BRACKET[open_bracket]:
             stack.pop()
         end += 1
+    if leave_brackets:
+        return text[start:end]
     return text[start + 1:end - 1]
 
 
