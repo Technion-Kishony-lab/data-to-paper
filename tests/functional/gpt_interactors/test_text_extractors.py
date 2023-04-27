29417e1844098012ac7282ef6e8ae12621e45a7e
Roy Kishony <rkishony@technion.ac.il>
2023-04-27 12:16:17 +0300

    remove obsolete tests

--- tests/functional/gpt_interactors/test_text_extractors.py
+++ tests/functional/gpt_interactors/test_text_extractors.py
@@ -1,5 +1,5 @@
 from scientistgpt.conversation.conversation import OPENAI_SERVER_CALLER
-from scientistgpt.gpt_interactors.text_extractors import TextExtractorGPT, extract_analysis_plan_from_response
+from scientistgpt.gpt_interactors.text_extractors import TextExtractorGPT
 
 
 @OPENAI_SERVER_CALLER.record_or_replay()
@@ -11,18 +11,3 @@ def test_text_extractor():
     assert 'I love machine learning' in extracted_text
     assert 'data science' not in extracted_text
     assert 'enjoying' not in extracted_text
-
-
-@OPENAI_SERVER_CALLER.record_or_replay()
-def test_extract_analysis_plan_from_response():
-    response = """
-    Here is the analysis plan:
-
-    1. Load the files
-    2. Clean the data
-    3. Analyze the data
-    """
-    extracted_text = extract_analysis_plan_from_response(response)
-    assert '1. Load the files' in extracted_text
-    assert '3. Analyze the data' in extracted_text
-    assert 'Here is' not in extracted_text
