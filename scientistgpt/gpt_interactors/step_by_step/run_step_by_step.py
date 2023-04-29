98bbab6575ee42423abc6c1db14788a140d7e849
Roy Kishony <rkishony@technion.ac.il>
2023-04-29 17:51:36 +0300

    add fake conversation of Student with Director to get the data and goal

--- scientistgpt/gpt_interactors/step_by_step/run_step_by_step.py
+++ scientistgpt/gpt_interactors/step_by_step/run_step_by_step.py
@@ -1,6 +1,7 @@
 from typing import Optional
 
 from scientistgpt.gpt_interactors.step_by_step.goal_and_plan import GoalReviewGPT, PlanReviewGPT
+from scientistgpt.gpt_interactors.step_by_step.user_to_student import DirectorToStudent
 from scientistgpt.gpt_interactors.step_by_step.write_code import CodeFeedbackGPT
 from scientistgpt.gpt_interactors.types import Products
 
@@ -8,13 +9,22 @@ from scientistgpt.gpt_interactors.types import Products
 def run_step_by_step(data_file_descriptions, research_goal: Optional[str] = None,
                      data_directory=None, output_directory=None) -> Products:
 
-    products = Products(data_file_descriptions=data_file_descriptions,
-                        research_goal=research_goal)
-    if research_goal is None:
-        products.research_goal = GoalReviewGPT(products).initialize_and_run_dialog()
-    products.analysis_plan = PlanReviewGPT(products).initialize_and_run_dialog()
+    products = Products()
 
-    products.code_and_output = CodeFeedbackGPT(products).get_analysis_code()
+    # Data file descriptions:
+    products.data_file_descriptions = DirectorToStudent(products=products).get_product_from_director(
+        product_field='data_file_descriptions', returned_product=data_file_descriptions)
+
+    # Goal
+    if research_goal is None:
+        products.research_goal = GoalReviewGPT(products=products).initialize_and_run_dialog()
+    else:
+        products.research_goal = DirectorToStudent(products=products).get_product_from_director(
+            product_field='research_goal', returned_product=research_goal)
+
+    products.analysis_plan = PlanReviewGPT(products=products).initialize_and_run_dialog()
+
+    products.code_and_output = CodeFeedbackGPT(products=products).get_analysis_code()
 
     products.result_summary = NotImplementedError
 
---
+++ scientistgpt/gpt_interactors/step_by_step/user_to_student.py
@@ -0,0 +1,26 @@
+from typing import Any
+
+from scientistgpt.cast import Agent
+from scientistgpt.gpt_interactors.step_by_step.goal_and_plan import BaseScientificGPT
+
+
+class DirectorToStudent(BaseScientificGPT):
+    """
+    Create a fake (predetermined) conversation, where the Student asks the Director (the application user) for products,
+    such as data description, or goal.
+    """
+    conversation_name: str = 'user-student'
+    assistant_agent: Agent = Agent.Director
+    user_agent: Agent = Agent.Student
+
+    def get_product_from_director(self, product_field: str, returned_product: Any):
+        """
+        Ask the user for a product, such as data description, or goal.
+        """
+        self.initialize_conversation_if_needed()
+        product_name = self.products.get_name(product_field)
+        self.apply_append_user_message(f'Hi, do you have a {product_name} for me?')
+        self.apply_append_surrogate_message(
+            f'Yes, here is the {product_name}:\n{returned_product}\n')
+        self.apply_append_user_message('Thank you!')
+        return returned_product
