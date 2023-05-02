304a42d42d8fa8b3d3ab5dac3b2e0843abbfa3e2
Roy Kishony <rkishony@technion.ac.il>
2023-05-02 22:15:31 +0300

    allow automatic choosing of attrs in Replacer

--- scientistgpt/utils/replacer.py
+++ scientistgpt/utils/replacer.py
@@ -6,10 +6,10 @@ from dataclasses import dataclass
 
 def with_attribute_replacement(func):
     """
-    a decorator for temporarily replace attributes of a Replacer class.
+    a decorator for methods of a Replacer class that should run while replacing attributes.
     """
     def wrapper(self, *args, **kwargs):
-        with self.replacing_attributes():
+        with self.attributes_replacement():
             return func(self, *args, **kwargs)
     return wrapper
 
@@ -23,6 +23,21 @@ def format_str_while_preserving_curly_brackets(string: str, **kwargs):
     for key, value in kwargs.items():
         string = re.sub(r"(?<!{){" + key + r"}(?!})", str(value), string)
     return string
+
+
+def _super_getatter(self, name):
+    return super(Replacer, self).__getattribute__(name)
+
+
+def _super_get_is_replacing(self):
+    return _super_getatter(self, 'is_replacing')
+
+
+def _do_not_replace(self):
+    """
+    A context manager for temporarily not replacing attributes of a Replacer class.
+    """
+    return super(Replacer, self).__getattribute__('not_replacing_attributes')()
 
 
 @dataclass
@@ -39,8 +54,8 @@ class Replacer:
 
     is_replacing: bool = False
 
-    REPLACED_ATTRS = []
-    ADDITIONAL_DICT_ATTRS = []
+    REPLACED_ATTRS = None  # type: tuple # e.g. ('greeting', 'name'), or None to replace all str attributes
+    ADDITIONAL_DICT_ATTRS = None  # type: tuple # e.g. ('age', ), or None to add all non-str attributes
 
     def _format_text(self, text):
         while True:
@@ -49,23 +64,45 @@ class Replacer:
             if text == old_text:
                 return text.format()
 
+    def get_replaced_attributes(self):
+        with _do_not_replace(self):
+            if self.REPLACED_ATTRS is not None:
+                return self.REPLACED_ATTRS
+            return [attr for attr in self.__dict__ if isinstance(_super_getatter(self, attr), str)]
+
+    def get_additional_dict_attributes(self):
+        with _do_not_replace(self):
+            if self.ADDITIONAL_DICT_ATTRS is not None:
+                return self.ADDITIONAL_DICT_ATTRS
+            return [attr for attr in self.__dict__ if not isinstance(_super_getatter(self, attr), str)
+                    and attr != 'is_replacing']
+
     def _get_formatting_dict(self):
-        old_is_replacing = self.is_replacing
-        self.is_replacing = False
-        dict_ = {attr: getattr(self, attr) for attr in self.REPLACED_ATTRS + self.ADDITIONAL_DICT_ATTRS}
-        self.is_replacing = old_is_replacing
-        return dict_
+        attributes_for_dict = (*self.get_replaced_attributes(), *self.get_additional_dict_attributes())
+        with self.not_replacing_attributes():
+            return {attr: getattr(self, attr) for attr in attributes_for_dict}
 
     def __getattribute__(self, item):
-        is_replacing = super().__getattribute__('is_replacing')
-        should_replace = item in super().__getattribute__('REPLACED_ATTRS')
-        if is_replacing and should_replace:
-            return self._format_text(super().__getattribute__(item))
-        return super().__getattribute__(item)
+        raw_value = _super_getatter(self, item)
+        if _super_get_is_replacing(self) and \
+                item in _super_getatter(self, 'get_replaced_attributes')():
+            return _super_getatter(self, '_format_text')(raw_value)
+        return raw_value
 
     @contextmanager
-    def replacing_attributes(self):
-        old_is_replacing = self.is_replacing
+    def attributes_replacement(self):
+        old_is_replacing = _super_get_is_replacing(self)
         self.is_replacing = True
-        yield
-        self.is_replacing = old_is_replacing
+        try:
+            yield
+        finally:
+            self.is_replacing = old_is_replacing
+
+    @contextmanager
+    def not_replacing_attributes(self):
+        old_is_replacing = _super_get_is_replacing(self)
+        self.is_replacing = False
+        try:
+            yield
+        finally:
+            self.is_replacing = old_is_replacing
