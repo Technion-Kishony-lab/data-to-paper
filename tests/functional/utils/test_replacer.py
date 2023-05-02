f7205c697d2d9b1090dbde491f5f9a86e099213f
Roy Kishony <rkishony@technion.ac.il>
2023-05-02 12:59:32 +0300

    refine replacer
    ccc

--- tests/functional/utils/test_replacer.py
+++ tests/functional/utils/test_replacer.py
@@ -5,21 +5,22 @@ from scientistgpt.utils.replacer import Replacer
 
 @dataclass
 class Greeter(Replacer):
-    REPLACED_ATTRS = ['greeting', 'name']
-    ADDITIONAL_DICT_ATTRS = ['name', 'adjective']
-    adjective = 'amazing'
+    REPLACED_ATTRS = ['greeting', 'name', 'adjective']
+    ADDITIONAL_DICT_ATTRS = ['age']
+    age: int = 20
+    adjective: str = 'amazing'
     name: str = 'the {adjective} john'
-    greeting: str = 'hello {name}'
+    greeting: str = 'hello, I am {name}, and I am {age} years old'
 
 
 def test_replacer():
     greeter = Greeter()
 
-    assert greeter.greeting == 'hello {name}'
+    assert greeter.greeting == 'hello, I am {name}, and I am {age} years old'
 
     greeter.is_replacing = True
-    assert greeter.greeting == 'hello the amazing john'
+    assert greeter.greeting == 'hello, I am the amazing john, and I am 20 years old'
 
     greeter.is_replacing = False
-    assert greeter.greeting == 'hello {name}'
+    assert greeter.greeting == 'hello, I am {name}, and I am {age} years old'
     assert greeter.name == 'the {adjective} john'
