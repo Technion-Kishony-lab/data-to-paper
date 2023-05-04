297961e68609e2ca806e2d22401f5348ff738c41
Roy Kishony <rkishony@technion.ac.il>
2023-05-04 20:03:51 +0300

    remove scientistgpt_project

--- scientistgpt_project/scientistgpt_app/routing.py
+++
@@ -1,7 +0,0 @@
-from django.urls import re_path
-
-from . import consumers
-
-websocket_urlpatterns = [
-    re_path(r"ws/scientistgpt/(?P<experiment_id>\w+)/$", consumers.ScientistGPTConsumer.as_asgi()),
-]
--- scientistgpt_project/scientistgpt_app/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-urlpatterns = [
-    path("", views.index, name="index"),
-    # in the tutorial, this is the example: path("<str:room_name>/", views.room, name="room"),
-    path("<str:experiment_id>/", views.index, name="scientistgpt"),
-    path('upload_data/<str:experiment_id>/', views.upload_data, name='upload_data'),
-]
