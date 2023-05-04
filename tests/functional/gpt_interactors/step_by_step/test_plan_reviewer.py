d58a3822ddbf6425c5db3cea0b992dd7baa8160a
Roy Kishony <rkishony@technion.ac.il>
2023-05-04 21:27:12 +0300

    move files

--- tests/functional/gpt_interactors/step_by_step/test_plan_reviewer.py
+++ tests/functional/gpt_interactors/step_by_step/test_plan_reviewer.py
@@ -2,11 +2,11 @@ from dataclasses import dataclass
 
 from _pytest.fixtures import fixture
 
-from g3pt.projects.scientific_research.paper_writing.types import ScientificProducts
+from g3pt.projects.scientific_research.types import ScientificProducts
 from g3pt.servers.chatgpt import OPENAI_SERVER_CALLER
 from g3pt.gpt_interactors.step_by_step.reviewers import GoalReviewGPT, PlanReviewGPT
 from g3pt.gpt_interactors.step_by_step.write_code import CodeFeedbackGPT
-from g3pt.gpt_interactors.types import Products, DataFileDescriptions, DataFileDescription
+from g3pt.gpt_interactors.types import DataFileDescriptions, DataFileDescription
 
 
 @dataclass(frozen=True)
