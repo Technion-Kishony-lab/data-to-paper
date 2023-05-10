878fd406bdc1ebed45df7aefca13e97c8f5b40f7
Roy Kishony <rkishony@technion.ac.il>
2023-05-10 09:11:06 +0300

    only remove repeated background messages, not other repeated messages

--- scientistgpt/scientistgpt/base_steps/base_products_conversers.py
+++ scientistgpt/scientistgpt/base_steps/base_products_conversers.py
@@ -27,12 +27,14 @@ class BaseProductsGPT(BaseProductsHandler, ConverserGPT):
 
     def _add_acknowledgement(self, product_field: str, is_last: bool = False):
         thank_you_message = self.product_acknowledgement.format(self.products.get_name(product_field))
-        self.apply_append_surrogate_message(thank_you_message)
+        self.apply_append_surrogate_message(
+            content=thank_you_message, tag=f'background_thanks_{product_field}', is_background=True)
         return thank_you_message
 
     def _add_product_description(self, product_field: str):
         product_description = self.products.get_description(product_field)
-        self.apply_append_user_message(product_description)
+        self.apply_append_user_message(
+            content=product_description, tag=f'background_{product_field}', is_background=True)
         return product_description
 
     def _pre_populate_background(self):
@@ -60,11 +62,13 @@ class BaseProductsReviewGPT(BaseProductsGPT, ReviewDialogDualConverserGPT):
         thank_you_message = super()._add_acknowledgement(product_field, is_last=is_last)
         if self.are_we_reviewing_at_all:
             thank_you_message += self.user_initiation_prompt if is_last else ''
-            self.apply_to_other_append_surrogate_message(thank_you_message)
+            self.apply_to_other_append_surrogate_message(
+                content=thank_you_message, tag=f'background_thanks_{product_field}', is_background=True)
         return thank_you_message
 
     def _add_product_description(self, product_field: str):
         product_description = super()._add_product_description(product_field)
         if self.are_we_reviewing_at_all:
-            self.apply_to_other_append_user_message(product_description)
+            self.apply_to_other_append_user_message(
+                content=product_description, tag=f'background_{product_field}', is_background=True)
         return product_description
