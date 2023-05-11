f0f0129f1f1e59d753f712c9205b2b01257eba58
Roy Kishony <rkishony@technion.ac.il>
2023-05-11 14:11:04 +0300

    set conversation (server-side)

--- scientistgpt/scientistgpt/projects/scientific_research/run_steps.py
+++ scientistgpt/scientistgpt/projects/scientific_research/run_steps.py
@@ -41,33 +41,35 @@ class ScientificStepsRunner(BaseStepsRunner):
                                                  user_agent=ScientificAgent.Performer,
                                                  conversation_name='with_director',
                                                  )
-        self.advance_stage(ScientificStage.DATA)
+        self.advance_stage_and_set_active_conversation(ScientificStage.DATA, ScientificAgent.Director)
         products.data_file_descriptions = director_converser.get_product_or_no_product_from_director(
             product_field='data_file_descriptions', returned_product=self.data_file_descriptions)
 
         # Goal
-        self.advance_stage(ScientificStage.GOAL)
+        self.advance_stage_and_set_active_conversation(ScientificStage.GOAL, ScientificAgent.Director)
         products.research_goal = director_converser.get_product_or_no_product_from_director(
             product_field='research_goal', returned_product=self.research_goal,
             acknowledge_no_product_message="OK. no problem. I will devise the goal myself.")
         if products.research_goal is None:
             # we did not get a goal from the director, so we need to devise it ourselves:
+            self.set_active_conversation(self.get_conversation_name_for_agent(ScientificAgent.Director))
             products.research_goal = self._get_converser(GoalReviewGPT).initialize_and_run_dialog()
 
         # Analysis plan
-        self.advance_stage(ScientificStage.PLAN)
+        self.advance_stage_and_set_active_conversation(ScientificStage.PLAN, ScientificAgent.PlanReviewer)
         products.analysis_plan = self._get_converser(PlanReviewGPT).initialize_and_run_dialog()
 
         # Code and output
-        self.advance_stage(ScientificStage.CODE)
+        self.advance_stage_and_set_active_conversation(ScientificStage.CODE, ScientificAgent.Debugger)
         products.code_and_output = self._get_converser(ScientificCodeProductsGPT).get_analysis_code()
 
         # Results interpretation
-        self.advance_stage(ScientificStage.INTERPRETATION)
+        self.advance_stage_and_set_active_conversation(
+            ScientificStage.INTERPRETATION, ScientificAgent.InterpretationReviewer)
         products.results_summary = self._get_converser(ResultsInterpretationReviewGPT).initialize_and_run_dialog()
 
         # Paper sections
-        self.advance_stage(ScientificStage.WRITING)
+        self.advance_stage_and_set_active_conversation(ScientificStage.WRITING, ScientificAgent.Writer)
         title_and_abstract_names = ['title', 'abstract']
         products.paper_sections['title'], products.paper_sections['abstract'] = \
             self._get_converser(TitleAbstractReviewGPT, section_names=title_and_abstract_names).get_sections()
@@ -78,13 +80,13 @@ class ScientificStepsRunner(BaseStepsRunner):
                     self._get_converser(PaperSectionReviewGPT, section_name=section_name).get_section()
 
         # Add citations to relevant paper sections
-        self.advance_stage(ScientificStage.CITATIONS)
+        self.advance_stage_and_set_active_conversation(ScientificStage.CITATIONS, ScientificAgent.CitationExpert)
         for section_name in SECTIONS_TO_ADD_CITATIONS_TO:
             products.cited_paper_sections[section_name] = \
                 self._get_converser(AddCitationReviewGPT, section_name=section_name).rewrite_section_with_citations()
 
         # Add tables to results section
-        self.advance_stage(ScientificStage.TABLES)
+        self.advance_stage_and_set_active_conversation(ScientificStage.TABLES, ScientificAgent.TableExpert)
         for section_name in SECTIONS_TO_ADD_TABLES_TO:
             products.paper_sections_with_tables[section_name] = \
                 self._get_converser(PaperSectionWithTablesReviewGPT, section_name=section_name).get_section()
