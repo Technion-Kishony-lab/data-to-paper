d1e25f25671f946a4cadccc42b746a8441d1fcb2
Roy Kishony <rkishony@technion.ac.il>
2023-05-12 03:57:59 +0300

    send_product_to_client
    sp
    
    
    send pr

--- scientistgpt/scientistgpt/projects/scientific_research/run_steps.py
+++ scientistgpt/scientistgpt/projects/scientific_research/run_steps.py
@@ -44,6 +44,7 @@ class ScientificStepsRunner(BaseStepsRunner):
         self.advance_stage_and_set_active_conversation(ScientificStage.DATA, ScientificAgent.Director)
         products.data_file_descriptions = director_converser.get_product_or_no_product_from_director(
             product_field='data_file_descriptions', returned_product=self.data_file_descriptions)
+        self.send_product_to_client(stage=ScientificStage.DATA, product_field='data_file_descriptions')
 
         # Goal
         self.advance_stage_and_set_active_conversation(ScientificStage.GOAL, ScientificAgent.Director)
@@ -54,19 +55,23 @@ class ScientificStepsRunner(BaseStepsRunner):
             # we did not get a goal from the director, so we need to devise it ourselves:
             self.set_active_conversation(ScientificAgent.Director)
             products.research_goal = self._get_converser(GoalReviewGPT).initialize_and_run_dialog()
+        self.send_product_to_client(stage=ScientificStage.GOAL, product_field='research_goal')
 
         # Analysis plan
         self.advance_stage_and_set_active_conversation(ScientificStage.PLAN, ScientificAgent.PlanReviewer)
         products.analysis_plan = self._get_converser(PlanReviewGPT).initialize_and_run_dialog()
+        self.send_product_to_client(stage=ScientificStage.PLAN, product_field='analysis_plan')
 
         # Code and output
         self.advance_stage_and_set_active_conversation(ScientificStage.CODE, ScientificAgent.Debugger)
         products.code_and_output = self._get_converser(ScientificCodeProductsGPT).get_analysis_code()
+        self.send_product_to_client(stage=ScientificStage.CODE, product_field='code_and_output')
 
         # Results interpretation
         self.advance_stage_and_set_active_conversation(
             ScientificStage.INTERPRETATION, ScientificAgent.InterpretationReviewer)
         products.results_summary = self._get_converser(ResultsInterpretationReviewGPT).initialize_and_run_dialog()
+        self.send_product_to_client(stage=ScientificStage.INTERPRETATION, product_field='results_summary')
 
         # Paper sections
         self.advance_stage_and_set_active_conversation(ScientificStage.WRITING, ScientificAgent.Writer)
@@ -78,18 +83,21 @@ class ScientificStepsRunner(BaseStepsRunner):
             if section_name not in title_and_abstract_names:
                 products.paper_sections[section_name] = \
                     self._get_converser(PaperSectionReviewGPT, section_name=section_name).get_section()
+        self.send_product_to_client(stage=ScientificStage.WRITING, product_field='paper_sections')
 
         # Add citations to relevant paper sections
         self.advance_stage_and_set_active_conversation(ScientificStage.CITATIONS, ScientificAgent.CitationExpert)
         for section_name in SECTIONS_TO_ADD_CITATIONS_TO:
             products.cited_paper_sections[section_name] = \
                 self._get_converser(AddCitationReviewGPT, section_name=section_name).rewrite_section_with_citations()
+        self.send_product_to_client(stage=ScientificStage.CITATIONS, product_field='cited_paper_sections')
 
         # Add tables to results section
         self.advance_stage_and_set_active_conversation(ScientificStage.TABLES, ScientificAgent.TableExpert)
         for section_name in SECTIONS_TO_ADD_TABLES_TO:
             products.paper_sections_with_tables[section_name] = \
                 self._get_converser(PaperSectionWithTablesReviewGPT, section_name=section_name).get_section()
+        self.send_product_to_client(stage=ScientificStage.TABLES, product_field='paper_sections_with_tables')
 
         paper_producer.assemble_compile_paper()
 
