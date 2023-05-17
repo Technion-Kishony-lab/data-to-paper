a7f90dc217beb7b936e41ff75d2e4ffa035fd957
Roy Kishony <rkishony@technion.ac.il>
2023-05-18 00:02:17 +0300

    pretty_section_names

--- scientistgpt/scientistgpt/base_steps/request_latex.py
+++ scientistgpt/scientistgpt/base_steps/request_latex.py
@@ -17,23 +17,26 @@ class BaseLatexProductsReviewGPT(BaseProductsReviewGPT):
     Option for removing citations from the section.
     Option for reviewing the sections (set max_review_turns > 0).
     """
-    ADDITIONAL_DICT_ATTRS = BaseProductsReviewGPT.ADDITIONAL_DICT_ATTRS | {'section_name'}
+    ADDITIONAL_DICT_ATTRS = BaseProductsReviewGPT.ADDITIONAL_DICT_ATTRS | {'section_name', 'pretty_section_names'}
     should_remove_citations_from_section = True
 
     # outputs:
-    section_contents: Union[str, List[str]] = field(default_factory=list)
-    section_names: Optional[NiceList[str]] = None
-
-    def __post_init__(self):
-        super().__post_init__()
-        if not isinstance(self.section_names, NiceList):
-            self.section_names = NiceList(self.section_names, wrap_with='', separator=', ', last_separator=' and ')
+    section_contents: List[str] = field(default_factory=list)
+    section_names: List[str] = field(default_factory=list)
 
     @property
     def section_name(self) -> Optional[str]:
         if len(self.section_names) == 1:
             return self.section_names[0]
         return None
+
+    @property
+    def pretty_section_names(self) -> NiceList[str]:
+        """
+        Return the section names capitalized.
+        """
+        return NiceList((section_name.title() for section_name in self.section_names),
+                        separator=', ', last_separator=' and ')
 
     def _check_self_response(self, response: str) -> Optional[str]:
         """
@@ -59,3 +62,6 @@ class BaseLatexProductsReviewGPT(BaseProductsReviewGPT):
     def get_sections(self) -> Union[str, list[str]]:
         self.initialize_and_run_dialog()
         return self.section_contents
+
+    def get_section(self):
+        return self.get_sections()[0]
