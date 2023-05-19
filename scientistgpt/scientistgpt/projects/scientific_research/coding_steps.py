07b3828fd6ca7c9b36be60552f80241ff91d581e
Roy Kishony <rkishony@technion.ac.il>
2023-05-19 11:55:14 +0300

    specify allowed files to use in each step

--- scientistgpt/scientistgpt/projects/scientific_research/coding_steps.py
+++ scientistgpt/scientistgpt/projects/scientific_research/coding_steps.py
@@ -39,32 +39,43 @@ class DataExplorationCodeProductsGPT(BaseScientificCodeProductsGPT):
     output_filename: str = 'data_exploration.txt'
     allow_creating_files: bool = True
 
-    output_content_prompt: str = dedent_triple_quote_str("""
-        The output file should be self-contained: any results you choose to save to this file \
-        should be accompanied with a short text explaining what they represent, and their \
-        respective units, if any.
-        
-        If you modify or add any new variables to the raw data, you should save the modified
-        dataset in a new file.
-        """)
-
-    code_mission: str = dedent_triple_quote_str("""
+    code_requesting_prompt: str = dedent_triple_quote_str("""
         As part of a data-exploration phase, please write a complete short Python code for getting a \
-        first sense of the data. For example, depending on the specific dataset, you might want to include:
+        first sense of the data. 
+        
+        Your code should create an output text file named "{actual_output_filename}", which should \
+        contain a summary of the data.
+        Depending on the specifics of the dataset, you might want to include:
 
-        * Measure of data scale (e.g., number of rows, number of columns)
+        * Measure of the scale of our data (e.g., number of rows, number of columns)
         * Translation of different units into a common sensible unit
         * Summary statistics of key variables
-        * List of most common values and abundances of categorical variables (if any) 
+        * List of most common values of categorical variables (if any) 
         * Counts of missing values
         * Any other data exploration analysis you deem relevant
+
+        The output file should be self-contained: any results you choose to save to this file \
+        should be accompanied with a short text header and indication of units (if any).
+
+        If needed, you can use the following packages which are already installed: {supported_packages}.
+        
+        Do not provide a sketch or pseudocode; write a complete runnable code.
+        Do not create any graphics, figures or any plots.
+        Do not send any presumed output examples.
+        
+        IMPORTANT: If you create a new dataframe or modify or add any new variables to the original dataframe, 
+        you should save the modified/new dataframes in new files.
         """)
+
     requesting_code_explanation_prompt: str = None
 
 
 @dataclass
 class DataAnalysisCodeProductsGPT(BaseScientificCodeProductsGPT):
-    ADDITIONAL_DICT_ATTRS = BaseScientificCodeProductsGPT.ADDITIONAL_DICT_ATTRS | {'available_input_files_description'}
+    ADDITIONAL_DICT_ATTRS = \
+        BaseScientificCodeProductsGPT.ADDITIONAL_DICT_ATTRS \
+        | {'description_of_additional_data_files_if_any', 'raw_data_filenames'}
+
     user_agent: ScientificAgent = ScientificAgent.Debugger
     conversation_name: str = 'data_analysis_code'
     code_name: str = 'Data Analysis'
@@ -76,9 +87,25 @@ class DataAnalysisCodeProductsGPT(BaseScientificCodeProductsGPT):
         analysis findings, summary statistics, etc.
         """)
     allow_creating_files: bool = False
-        Write a complete short Python code to achieve the goal specified above.
-        {available_input_files_description}
+    code_requesting_prompt: str = dedent_triple_quote_str("""
+        Write a complete short Python code to achieve the research goal specified above.
+        
+        As input, you can load the following raw data files:
+        {raw_data_filenames}
+        {description_of_additional_data_files_if_any}
+        
+        Don't provide a sketch or pseudocode; write a complete runnable code.
+        If needed, you can use the following packages in your code: `numpy`, `pandas` and `scipy`.
+        
+        The output of your code should be a text file named "{actual_output_filename}".
+        All results we may need for a scientific paper should be saved to this text file,
+        including analysis findings, summary statistics, statistical tests, etc.
+        
+        Do not write to any other files.
+        Do not create any graphics, figures or any plots.
+        Do not send any presumed output examples.
         """)
+
     requesting_code_explanation_prompt: str = dedent_triple_quote_str("""
         Please explain what your code does. Do not provide a line-by-line explanation, rather provide a \
         high-level explanation of the code in a language suitable for a Methods section of a research \
@@ -86,13 +113,23 @@ class DataAnalysisCodeProductsGPT(BaseScientificCodeProductsGPT):
         """)
 
     @property
-    def available_input_files_description(self) -> str:
-        if self.products.data_exploration_code_and_output is None:
-            return ''
+    def raw_data_filenames(self) -> NiceList[str]:
+        return NiceList(super().data_filenames, wrap_with='"')
 
-        created_data_files = self.products.data_exploration_code_and_output.get_created_files_beside_output_file()
-        if len(created_data_files) == 0:
+    @property
+    def files_created_during_data_exploration(self) -> NiceList[str]:
+        if self.products.data_exploration_code_and_output is None:
+            return NiceList()
+        return NiceList(self.products.data_exploration_code_and_output.get_created_files_beside_output_file(),
+                        wrap_with='"')
+
+    @property
+    def data_filenames(self) -> NiceList[str]:
+        return NiceList(self.raw_data_filenames + self.files_created_during_data_exploration)
+
+    @property
+    def description_of_additional_data_files_if_any(self) -> str:
+        if len(self.files_created_during_data_exploration) == 0:
             return ''
-        return f'The following files are available at the code folder:\n' \
-               f'The raw data files: {self.data_filenames}.\n' \
-               f'The files created by the data exploration code: {created_data_files}.\n'
+        return f'Or you can also use the processed files created above by the data exploration code:\n' \
+               f'{self.files_created_during_data_exploration}'
