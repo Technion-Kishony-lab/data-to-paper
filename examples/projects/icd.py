d4c26c74469fd258588604076f5dff88f7152e40
Roy Kishony <rkishony@technion.ac.il>
2023-05-21 22:50:51 +0300

    major refactor of projects

---
+++ examples/projects/icd.py
@@ -0,0 +1,9 @@
+from examples.run_project import get_paper
+
+get_paper(
+    project='icd',
+    files=[],
+    research_goal=None,
+    output_folder='out1',
+    should_do_data_exploration=True,
+    should_mock_servers=True)
