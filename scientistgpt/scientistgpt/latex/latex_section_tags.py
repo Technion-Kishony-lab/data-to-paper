8cd20f61fbf5aba9cf4226bf195d923483caaace
Tal Ifargan <talifargan@gmail.com>
2023-05-22 00:58:06 +0300

    added `table` tag to list of possible latex extraction tags

--- scientistgpt/scientistgpt/latex/latex_section_tags.py
+++ scientistgpt/scientistgpt/latex/latex_section_tags.py
@@ -2,18 +2,19 @@ from typing import List, Dict
 
 from scientistgpt.utils.tag_pairs import TagPairs
 
-SECTIONS_TO_TAG_PAIR_OPTIONS: Dict[str, List[TagPairs]] = {
+SECTIONS_OR_FRAGMENTS_TO_TAG_PAIR_OPTIONS: Dict[str, List[TagPairs]] = {
     'title': [TagPairs('\\title{', '}')],
-    'abstract': [TagPairs('\\begin{abstract}', '\\end{abstract}')]
+    'abstract': [TagPairs('\\begin{abstract}', '\\end{abstract}')],
+    'table': [TagPairs('\\begin{table}', '\\end{table}')]
 }
 
 
-def get_list_of_tag_pairs_for_section(section: str) -> List[TagPairs]:
+def get_list_of_tag_pairs_for_section_or_fragment(section_or_fragment: str) -> List[TagPairs]:
     """
     Get a list with different options for tag pairs enclosing the specified section.
     """
-    if section in SECTIONS_TO_TAG_PAIR_OPTIONS:
-        return SECTIONS_TO_TAG_PAIR_OPTIONS[section]
+    if section_or_fragment in SECTIONS_OR_FRAGMENTS_TO_TAG_PAIR_OPTIONS:
+        return SECTIONS_OR_FRAGMENTS_TO_TAG_PAIR_OPTIONS[section_or_fragment]
     else:
-        return [TagPairs(f'\\section{{{section.capitalize()}}}', None),
-                TagPairs(f'\\section*{{{section.capitalize()}}}', None)]
+        return [TagPairs(f'\\section{{{section_or_fragment.capitalize()}}}', None),
+                TagPairs(f'\\section*{{{section_or_fragment.capitalize()}}}', None)]
