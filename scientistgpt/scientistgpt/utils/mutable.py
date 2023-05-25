b1248cad2ed7ef14907708e1c9e1de5bed463159
Roy Kishony <rkishony@technion.ac.il>
2023-05-26 01:23:43 +0300

    make env parameters Mutable

---
+++ scientistgpt/scientistgpt/utils/mutable.py
@@ -0,0 +1,45 @@
+import contextlib
+from dataclasses import dataclass
+from typing import Any
+
+
+@dataclass
+class Mutable:
+    val: Any
+
+    def set(self, val: Any):
+        self.val = val
+
+    @contextlib.contextmanager
+    def temporary_set(self, val):
+        current = self.val
+        self.set(val)
+        try:
+            yield
+        finally:
+            self.set(current)
+
+    def __eq__(self, other):
+        return self.val == other
+
+    def __ne__(self, other):
+        return self.val != other
+
+    def __lt__(self, other):
+        return self.val < other
+
+    def __le__(self, other):
+        return self.val <= other
+
+    def __gt__(self, other):
+        return self.val > other
+
+    def __ge__(self, other):
+        return self.val >= other
+
+
+@dataclass
+class Flag(Mutable):
+
+    def __bool__(self):
+        return self.val
