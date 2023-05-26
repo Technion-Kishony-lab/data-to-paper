2c23d852c934a5c5aac34942236f982b3ad9de8c
Roy Kishony <rkishony@technion.ac.il>
2023-05-27 00:29:30 +0300

    run_parameters
    icd

--- examples/projects/covid.py
+++ examples/projects/covid.py
@@ -1,8 +1,13 @@
 from examples.run_project import get_paper
 
-get_paper(project='covid',
-          data_filenames=["eng_covid_vaccine_side_effects.csv"],
-          research_goal=None,
-          output_folder='out1',
-          should_do_data_exploration=True,
-          should_mock_servers=True)
+RUN_PARAMETERS = dict(
+    project='covid',
+    data_filenames=["eng_covid_vaccine_side_effects.csv"],
+    research_goal=None,
+    should_do_data_exploration=True,
+)
+
+if __name__ == '__main__':
+    get_paper(**RUN_PARAMETERS,
+              output_folder='out1',
+              should_mock_servers=True)
