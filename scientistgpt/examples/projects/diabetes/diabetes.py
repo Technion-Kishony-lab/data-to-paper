43056b2d977ecfaa45c914641833ba13b1347825
Roy Kishony <rkishony@technion.ac.il>
2023-05-28 01:26:51 +0300

    EXAMPLE: '2023-05-26 Nice classifiers results' - works

--- scientistgpt/examples/projects/diabetes/diabetes.py
+++ scientistgpt/examples/projects/diabetes/diabetes.py
@@ -1,10 +1,12 @@
 from examples.run_project import get_paper
 
+goal = "Using machine learning models and multivariate analysis find risk factors for diabetes. " \
+       "Build predictive models to predict diabetes from health indicators.",
+
 RUN_PARAMETERS = dict(
     project='diabetes',
     data_filenames=["diabetes_binary_health_indicators_BRFSS2015.csv"],
-    research_goal="Using machine learning models and multivariate analysis find risk factors for diabetes. "
-                  "Build predictive models to predict diabetes from health indicators.",
+    research_goal=None,
     should_do_data_exploration=True,
 )
 
