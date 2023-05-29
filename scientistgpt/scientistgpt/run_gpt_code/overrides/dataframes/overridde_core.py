9cbc2ad68740c0e1f09c617878b2158a6f4a291e
Roy Kishony <rkishony@technion.ac.il>
2023-05-29 11:17:07 +0300

    completely redo dataframe overriding. now replacing methods in frame.DataFrame

--- scientistgpt/scientistgpt/run_gpt_code/overrides/dataframes/overridde_core.py
+++ scientistgpt/scientistgpt/run_gpt_code/overrides/dataframes/overridde_core.py
@@ -1,20 +1,43 @@
+import pandas as pd
+from pandas.core.frame import DataFrame
+
+from scientistgpt.utils.mutable import Mutable
 from scientistgpt.utils.singleton import run_once
 from pandas.core.generic import NDFrame
-from .dataframe_operations import SaveDataframeOperation
+from .dataframe_operations import SaveDataframeOperation, CreationDataframeOperation, DataframeOperation, \
+    ChangeSeriesDataframeOperation, AddSeriesDataframeOperation, RemoveSeriesDataframeOperation
 
-original_to_csv = NDFrame.to_csv
-original_str = NDFrame.__str__
+ON_CHANGE = Mutable(None)
+
+original_init = DataFrame.__init__
+original_to_csv = DataFrame.to_csv
+original_str = DataFrame.__str__
+original_setitem = DataFrame.__setitem__
+original_delitem = DataFrame.__delitem__
 
 
-def to_csv(self, *args, **kwargs):
-    from .override_dataframe import ReportingDataFrame
-    on_change = ReportingDataFrame.ON_CHANGE
-    result = original_to_csv(self, *args, **kwargs)
-    if on_change is not None:
-        file_path = args[0] if len(args) > 0 else kwargs.get('path_or_buf')
-        columns = list(self.columns.values) if hasattr(self, 'columns') else None
-        on_change(self, SaveDataframeOperation(id=id(self), file_path=file_path, columns=columns))
-    return result
+def _notify_on_change(self, operation: DataframeOperation):
+    if ON_CHANGE.val is not None:
+        ON_CHANGE.val(self, operation)
+
+
+def __init__(self, *args, created_by: str = None, file_path: str = None, **kwargs):
+    original_init(self, *args, **kwargs)
+    self.created_by = created_by
+    self.file_path = file_path
+    _notify_on_change(self, CreationDataframeOperation(
+        id=id(self), created_by=created_by, file_path=file_path, columns=list(self.columns.values)))
+
+
+def __setitem__(self, key, value):
+    operation_type = ChangeSeriesDataframeOperation if key in self else AddSeriesDataframeOperation
+    original_setitem(self, key, value)
+    _notify_on_change(self, operation_type(id=id(self), series_name=key))
+
+
+def __delitem__(self, key):
+    original_delitem(self, key)
+    _notify_on_change(self, RemoveSeriesDataframeOperation(id=id(self), series_name=key))
 
 
 def __str__(self):
@@ -22,10 +45,27 @@ def __str__(self):
     return self.to_string()
 
 
+def to_csv(self, *args, **kwargs):
+    result = original_to_csv(self, *args, **kwargs)
+    file_path = args[0] if len(args) > 0 else kwargs.get('path_or_buf')
+    columns = list(self.columns.values) if hasattr(self, 'columns') else None
+    _notify_on_change(self, SaveDataframeOperation(id=id(self), file_path=file_path, columns=columns))
+    return result
+
+
+FUNC_NAMES_TO_FUNCS = {
+    '__init__': __init__,
+    '__setitem__': __setitem__,
+    '__delitem__': __delitem__,
+    '__str__': __str__,
+    'to_csv': to_csv,
+}
+
+
 @run_once
 def override_core_ndframe():
     """
     Overrides the pandas DataFrame class to report changes in the data frame.
     """
-    NDFrame.__str__ = __str__
-    NDFrame.to_csv = to_csv
+    for func_name, func in FUNC_NAMES_TO_FUNCS.items():
+        setattr(DataFrame, func_name, func)
