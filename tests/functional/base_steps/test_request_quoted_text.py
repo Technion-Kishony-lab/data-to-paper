cfd6968d5868ed697410812b2701155da8282a13
Roy Kishony <rkishony@technion.ac.il>
2023-06-03 19:41:26 +0300

    test request_quoted. fix bump up

---
+++ tests/functional/base_steps/test_request_quoted_text.py
@@ -0,0 +1,55 @@
+from scientistgpt.base_steps import BaseProductsQuotedReviewGPT
+from dataclasses import dataclass
+from typing import Dict, Any, List, Set
+
+import pytest
+
+from scientistgpt.env import MAX_MODEL_ENGINE
+from scientistgpt.servers.chatgpt import OPENAI_SERVER_CALLER
+from scientistgpt.servers.openai_models import ModelEngine
+from scientistgpt.utils.types import ListBasedSet
+
+from .utils import TestProductsReviewGPT, check_wrong_and_right_responses
+
+
+@dataclass
+class TestBaseProductsQuotedReviewGPT(TestProductsReviewGPT, BaseProductsQuotedReviewGPT):
+    pass
+
+
+enclosed_text = "\nThis is the enclosed text\n"
+
+
+@pytest.mark.parametrize('quotes', ['"""', "'''", '```'])
+def test_request_quoted_text(quotes):
+    check_wrong_and_right_responses(
+        responses=[f'Here is the text:\n{quotes}{enclosed_text}{quotes}\nShould be all good.'],
+        requester=TestBaseProductsQuotedReviewGPT(),
+        correct_value=enclosed_text)
+
+
+@pytest.mark.parametrize('incorrect_quotes', [
+    ('"""', ''),
+    ('', ''),
+])
+def test_request_quoted_text_with_error(incorrect_quotes):
+    check_wrong_and_right_responses(
+        responses=[f'Here is some wrongly enclosed test:\n{incorrect_quotes[0]}{enclosed_text}{incorrect_quotes[1]}\nCheck it out.',
+                   f'Now it is good:\n```{enclosed_text}```\n'],
+        requester=TestBaseProductsQuotedReviewGPT(),
+        correct_value=enclosed_text,
+        error_texts=("enclosed within triple-backticks", ))
+
+
+def test_request_quoted_text_bumps_model():
+    with OPENAI_SERVER_CALLER.mock(
+            ['I am not sending any enclosed text',
+             'I am starting to write, but fails: \n```\nthe secret recipe is\n',
+             'Now, as a bumped-up model, I can finish this though: \n```\nthe secret recipe is to add ton of chocolate\n```'],
+            record_more_if_needed=False):
+        requester = TestBaseProductsQuotedReviewGPT(model_engine=ModelEngine.GPT35_TURBO)
+        assert requester.run_dialog_and_get_valid_result() == '\nthe secret recipe is to add ton of chocolate\n'
+
+    # assert context as sent to the server:
+    models_used = [h[1].get('model_engine', None) for h in OPENAI_SERVER_CALLER.args_kwargs_response_history]
+    assert models_used == [ModelEngine.GPT35_TURBO, ModelEngine.GPT35_TURBO, MAX_MODEL_ENGINE]
