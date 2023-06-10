14451b055c02d128fd5f4a5e4d480195dadcc767
Roy Kishony <rkishony@technion.ac.il>
2023-06-10 16:12:21 +0300

    fix feedback on ambiguous reviewer

--- scientistgpt/scientistgpt/utils/text_counting.py
+++ scientistgpt/scientistgpt/utils/text_counting.py
@@ -13,3 +13,10 @@ def line_count(text: str) -> int:
     Count the number of lines in provided test.
     """
     return len(text.splitlines())
+
+
+def is_bulleted_list(text: str) -> bool:
+    """
+    Check if the provided text is a bulleted list, i.e. that there are lines starting with '- ' or '* '.
+    """
+    return any(line.startswith('- ') or line.startswith('* ') for line in text.splitlines())
