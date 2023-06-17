0691dc81672a1bb2967eaa6a0f920efb4fadc966
Roy Kishony <rkishony@technion.ac.il>
2023-06-17 23:07:51 +0300

    check that SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER is not sensitive to paper_id

--- tests/functional/servers/test_semantic_scholar_server.py
+++ tests/functional/servers/test_semantic_scholar_server.py
@@ -1,3 +1,4 @@
+import numpy as np
 from _pytest.fixtures import fixture
 from data_to_paper.servers.semantic_scholar import SEMANTIC_SCHOLAR_SERVER_CALLER, \
     SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER
@@ -9,8 +10,10 @@ def query():
 
 @fixture()
 def paper():
-    return {"paper_id": "123", "title": "Hospital outbreak of Middle East respiratory syndrome",
-             "abstract": "Between April 1 and May 23, 2013, a total of 23 cases of MERS-CoV ...",}
+    return {"paper_id": "",
+            "title": "Hospital outbreak of Middle East respiratory syndrome",
+            "abstract": "Between April 1 and May 23, 2013, a total of 23 cases of MERS-CoV ...",
+            }
 
 
 def test_semantic_scholar_get_papers(query):
@@ -19,5 +22,8 @@ def test_semantic_scholar_get_papers(query):
 
 
 def test_semantic_scholar_get_paper_embedding(paper):
-    embedding = SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER.get_server_response(paper)
-    assert len(embedding) > 0
+    embedding1 = SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER.get_server_response(paper)
+    paper["paper_id"] = "123"
+    embedding2 = SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER.get_server_response(paper)
+    assert len(embedding1) > 0
+    assert np.array_equal(embedding1, embedding2)
