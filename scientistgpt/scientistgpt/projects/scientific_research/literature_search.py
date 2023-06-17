059ffffb7989099d3b13251cfa5e576061048be2
Roy Kishony <rkishony@technion.ac.il>
2023-06-17 09:06:07 +0300

    goal-specific lit search

--- scientistgpt/scientistgpt/projects/scientific_research/literature_search.py
+++ scientistgpt/scientistgpt/projects/scientific_research/literature_search.py
@@ -11,12 +11,14 @@ from scientistgpt.utils.nice_list import NiceDict, NiceList
 
 
 @dataclass
-class LiteratureSearchReviewGPT(PythonDictWithDefinedKeysReviewBackgroundProductsConverser):
-    step: str = None
+class GoalLiteratureSearchReviewGPT(PythonDictWithDefinedKeysReviewBackgroundProductsConverser):
+    step: str = 'goal'
+    number_of_papers_per_query: int = 7
     max_reviewing_rounds: int = 0
-    requested_keys: Iterable[str] = ('background', 'dataset', 'methods', 'questions', 'results')
-    background_product_fields: Tuple[str, ...] = ('research_goal', 'title_and_abstract')
-    conversation_name: str = 'literature_search'
+    requested_keys: Iterable[str] = ('dataset', 'questions', )
+    background_product_fields: Tuple[str, ...] = ('research_goal', 'hypothesis_testing_plan')
+    conversation_name: str = 'literature_search_goal'
+    is_new_conversation: bool = None
     value_type: type = Dict[str, List[str]]
     goal_noun: str = 'literature search queries'
     goal_verb: str = 'write'
@@ -25,33 +27,27 @@ class LiteratureSearchReviewGPT(PythonDictWithDefinedKeysReviewBackgroundProduct
     user_initiation_prompt: str = dedent_triple_quote_str("""
         Please write literature-search queries that we can use to search for papers related to our study.
         
-        You would need to compose search queries to identify prior papers covering 4 areas:
+        You would need to compose search queries to identify prior papers covering these 2 areas:
         
-        "background": papers that provide background on the overall subject of our study
         "dataset": papers that use the same or similar datasets as in our study
-        "methods": papers that use the same or similar methods as in our study
         "questions": papers that ask questions similar to our study
-        "results": papers reporting results similar or related to our results
 
-        Return your answer as a `Dict[str, List[str]]`, where the keys are the 5 areas noted above, \
-        and the values are lists of query string. Each individual query should be a string with up to 7 words. 
+        Return your answer as a `Dict[str, List[str]]`, where the keys are the 2 areas noted above, \
+        and the values are lists of query string. Each individual query should be a string with up to 5-10 words. 
         
         For example, for a study reporting waning of the efficacy of the covid-19 BNT162b2 vaccine based on analysis \
         of the real-world UK National Core dataset, the queries could be:  
         {
-            "background": ["SARS-CoV2 spread", "covid-19 global impact", "covid-19 vaccine"]
-            "dataset": ["covid-19 vaccine real-world data", "UK National Core covid-19 dataset"]
-            "methods": ["covid-19 vaccine efficacy analysis", "kaplan-meier survival analysis"]
-            "questions": ["covid-19 vaccine efficacy", "covid-19 vaccine efficacy over time"]
-            "results": ["covid-19 vaccine efficacy waning"]
+            "dataset": ["covid-19 vaccine real-world data", "The UK National Core covid-19 dataset"]
+            "questions": ["covid-19 vaccine efficacy", "covid-19 vaccine efficacy over time", "covid-19 vaccine waning"]
         }
         """)
 
     def _check_response_value(self, response_value: dict) -> NiceDict:
         for queries in response_value.values():
             for query in queries:
-                if word_count(query) > 12:
-                    self._raise_self_response_error('queries should be up to 7 word long')
+                if word_count(query) > 10:
+                    self._raise_self_response_error('queries should be 5-10 word long')
         return NiceDict({k: NiceList(v, wrap_with='"', prefix='[\n' + ' ' * 8, suffix='\n' + ' ' * 4 + ']',
                                      separator=',\n' + ' ' * 8)
                          for k, v in response_value.items()})
@@ -62,5 +58,37 @@ class LiteratureSearchReviewGPT(PythonDictWithDefinedKeysReviewBackgroundProduct
         for scope, queries in scopes_to_list_of_queries.items():
             for query in queries:
                 literature_search[LiteratureSearchParams(step=self.step, scope=scope, query=query)] = \
-                    SEMANTIC_SCHOLAR_SERVER_CALLER.get_server_response(query)
+                    SEMANTIC_SCHOLAR_SERVER_CALLER.get_server_response(query, rows=self.number_of_papers_per_query)
         return NiceDict(literature_search)
+
+
+@dataclass
+class WritingLiteratureSearchReviewGPT(GoalLiteratureSearchReviewGPT):
+    step: str = 'writing'
+    requested_keys: Iterable[str] = ('background', 'dataset', 'methods', 'questions', 'results')
+    background_product_fields: Tuple[str, ...] = ('research_goal', 'hypothesis_testing_plan', 'title_and_abstract')
+    conversation_name: str = 'literature_search_writing'
+    user_initiation_prompt: str = dedent_triple_quote_str("""
+        Please write literature-search queries that we can use to search for papers related to our study.
+
+        You would need to compose search queries to identify prior papers covering these 5 areas:
+
+        "background": papers that provide background on the overall subject of our study
+        "dataset": papers that use the same or similar datasets as in our study
+        "methods": papers that use the same or similar methods as in our study
+        "questions": papers that ask questions similar to our study
+        "results": papers reporting results similar or related to our results
+
+        Return your answer as a `Dict[str, List[str]]`, where the keys are the 5 areas noted above, \
+        and the values are lists of query string. Each individual query should be a string with up to 5-10 words. 
+
+        For example, for a study reporting waning of the efficacy of the covid-19 BNT162b2 vaccine based on analysis \
+        of the real-world UK National Core dataset, the queries could be:  
+        {
+            "background": ["SARS-CoV2 spread", "covid-19 global impact", "covid-19 vaccine"]
+            "dataset": ["covid-19 vaccine real-world data", "The UK National Core covid-19 dataset"]
+            "methods": ["covid-19 vaccine efficacy analysis", "kaplan-meier survival analysis"]
+            "questions": ["covid-19 vaccine efficacy", "covid-19 vaccine efficacy over time"]
+            "results": ["covid-19 vaccine efficacy waning"]
+        }
+        """)
