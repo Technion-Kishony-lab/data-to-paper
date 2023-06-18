821ab52bd1163f1813678694cf756760f43b5dbb
Tal Ifargan <talifargan@gmail.com>
2023-06-18 13:56:42 +0300

    add too wide table or section after compilation check

--- data_to_paper/data_to_paper/latex/exceptions.py
+++ data_to_paper/data_to_paper/latex/exceptions.py
@@ -16,19 +16,21 @@ class FailedToExtractLatexContent(data_to_paperException, ValueError):
 
 
 @dataclass
-class LatexCompilationError(data_to_paperException, ValueError):
+class LatexProblemInCompilation(data_to_paperException, ValueError):
     """
     Raised when the latex content could not be compiled.
     """
     latex_content: str
     pdflatex_output: str
+    problem_starting_term: str
 
     def _extract_error_message(self) -> str:
         """
         Extract the error message from the pdflatex output.
         """
         lines = self.pdflatex_output.splitlines()
-        first_line_of_error_message = next((i for i, line in enumerate(lines) if line.startswith('! ')), None)
+        first_line_of_error_message = next((i for i, line in enumerate(lines) if
+                                            line.startswith(self.problem_starting_term)), None)
         return '\n'.join(lines[first_line_of_error_message:first_line_of_error_message + 4])
 
     def get_latex_exception_line_number(self) -> Optional[int]:
@@ -48,6 +50,27 @@ class LatexCompilationError(data_to_paperException, ValueError):
         if error_line is None:
             return None
         return '\n'.join(self.latex_content.splitlines()[error_line - 1:error_line + 2])
+
+    def __str__(self):
+        erroneous_lines = self._get_erroneous_lines()
+        if erroneous_lines is None:
+            return f'Problem to successfully compile latex due to the following problem or error:\n\n' \
+                   f'```\n' \
+                   f'{self._extract_error_message()}' \
+                   f'```\n'
+        return f'Problem to successfully compile latex due to these lines:\n{self._get_erroneous_lines()}\n\n' \
+               f'Got the following problem or error:\n' \
+               f'```\n' \
+               f'{self._extract_error_message()}' \
+               f'```\n'
+
+
+@dataclass
+class LatexCompilationError(LatexProblemInCompilation):
+    """
+    Raised when the latex content could not be compiled.
+    """
+    problem_starting_term: str = '! '
 
     def __str__(self):
         erroneous_lines = self._get_erroneous_lines()
@@ -72,3 +95,27 @@ class UnwantedCommandsUsedInLatex(data_to_paperException, ValueError):
 
     def __str__(self):
         return f'Unwanted commands used in latex:\n{self.unwanted_commands}\n\n'
+
+
+@dataclass
+class TooWideTableOrText(LatexProblemInCompilation):
+    """
+    Raised when the latex content contains a table or text that is too wide (Overfull \hbox).
+    """
+    problem_starting_term: str = r'Overfull \hbox '
+
+    def __str__(self):
+        erroneous_lines = self._get_erroneous_lines()
+        if erroneous_lines is None:
+            return f'There was a problem with the latex compilation, the table or section you wrote was to wide ' \
+                   f'to fit within the text width:\n\n' \
+                   f'```\n' \
+                   f'{self._extract_error_message()}' \
+                   f'```\n' \
+                   f'Try to shorten the text in the table or section you wrote or drop unnecessary columns.\n\n'
+        return f'There was a problem with the latex compilation in these lines:\n{self._get_erroneous_lines()}\n\n' \
+               f'The table or section you wrote was to wide to fit within the text width:\n' \
+               f'```\n' \
+               f'{self._extract_error_message()}' \
+               f'```\n' \
+               f'Try to shorten the text in the table or section you wrote or drop unnecessary columns.\n\n'
