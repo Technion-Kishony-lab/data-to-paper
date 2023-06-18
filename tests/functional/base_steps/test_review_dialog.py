3d9a10205031a7c7f29e7f1e1e2f5c664c4b073e
Tal Ifargan <talifargan@gmail.com>
2023-06-18 19:07:58 +0300

    skip ambiguous reviewer test

--- tests/functional/base_steps/test_review_dialog.py
+++ tests/functional/base_steps/test_review_dialog.py
@@ -1,5 +1,6 @@
 from dataclasses import dataclass
 
+import pytest
 
 from data_to_paper.base_steps.dual_converser import ReviewDialogDualConverserGPT
 
@@ -27,7 +28,7 @@ def test_review_cycle():
         assert requester.run_dialog_and_get_valid_result() == 'Thank you. Here is my improved version'
     assert len(requester.conversation) == 3
 
-
+@pytest.mark.skip(reason="Currently, we don't handle ambiguous responses using chatgpt")
 def test_ambiguous_reviewer():
     requester = TestReviewDialogDualConverserGPT()
     with OPENAI_SERVER_CALLER.mock([
