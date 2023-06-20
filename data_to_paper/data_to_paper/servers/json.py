c69a9d9bc67f7c1b3276cba52282f1ab87f56201
Roy Kishony <rkishony@technion.ac.il>
2023-06-20 16:41:45 +0300

    organize json save/load

--- data_to_paper/data_to_paper/servers/json.py
+++ data_to_paper/data_to_paper/servers/json.py
@@ -3,42 +3,54 @@ import json
 import openai
 
 
-def save_to_json(data: list, filename):
-    serialized_data = []
-    for item in data:
-        if isinstance(item, Exception):
-            item = {
-                'is_exception': True,
-                'type': type(item).__name__,
-                'args': item.args,
-            }
-        serialized_data.append(item)
+def _serialize_exception(exception):
+    return {
+        'is_exception': True,
+        'type': type(exception).__name__,
+        'args': exception.args,
+    }
 
+
+def _serialize_item(item):
+    if isinstance(item, Exception):
+        item = _serialize_exception(item)
+    return item
+
+
+def _de_serialize_item(item):
+    if isinstance(item, dict) and 'is_exception' in item and item['is_exception'] is True:
+        exception_type = item['type']
+        args = tuple(item['args'])
+        # if exception in builtins:
+        if hasattr(builtins, exception_type):
+            exception = getattr(builtins, exception_type)(*args)
+        elif hasattr(openai.error, exception_type):
+            if exception_type == 'InvalidRequestError':
+                exception = getattr(openai.error, exception_type)(*args, param=None)
+            else:
+                exception = getattr(openai.error, exception_type)(*args)
+        else:
+            exception = Exception(*args)
+        return exception
+    else:
+        return item
+
+
+def _dump_to_json(serialized_data, filename):
     with open(filename, 'w') as file:
         json.dump(serialized_data, file, indent=4, sort_keys=True)
 
 
-def load_from_json(filename):
+def _load_from_json(filename):
     with open(filename, 'r') as file:
-        serialized_data = json.load(file)
+        return json.load(file)
 
-    data = []
-    for item in serialized_data:
-        if isinstance(item, dict) and 'is_exception' in item and item['is_exception'] is True:
-            exception_type = item['type']
-            args = tuple(item['args'])
-            # if exception in builtins:
-            if hasattr(builtins, exception_type):
-                exception = getattr(builtins, exception_type)(*args)
-            elif hasattr(openai.error, exception_type):
-                if exception_type == 'InvalidRequestError':
-                    exception = getattr(openai.error, exception_type)(*args, param=None)
-                else:
-                    exception = getattr(openai.error, exception_type)(*args)
-            else:
-                exception = Exception(*args)
-            data.append(exception)
-        else:
-            data.append(item)
 
-    return data
+def save_list_to_json(data: list, filename):
+    serialized_data = [_serialize_item(item) for item in data]
+    _dump_to_json(serialized_data, filename)
+
+
+def load_list_from_json(filename):
+    serialized_data = _load_from_json(filename)
+    return [_de_serialize_item(item) for item in serialized_data]
