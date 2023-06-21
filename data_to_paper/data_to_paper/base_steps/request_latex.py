8a585dc2be555a867916586d0071ab31b493ac05
Tal Ifargan <talifargan@gmail.com>
2023-06-21 20:10:15 +0300

    check for non latex citations in section

--- data_to_paper/data_to_paper/base_steps/request_latex.py
+++ data_to_paper/data_to_paper/base_steps/request_latex.py
@@ -10,9 +10,9 @@ from data_to_paper.utils.text_formatting import wrap_text_with_triple_quotes
 from data_to_paper.utils.file_utils import get_non_existing_file_name
 
 from data_to_paper.latex import FailedToExtractLatexContent, extract_latex_section_from_response
-from data_to_paper.latex.exceptions import UnwantedCommandsUsedInLatex, LatexProblemInCompilation
+from data_to_paper.latex.exceptions import UnwantedCommandsUsedInLatex, LatexProblemInCompilation, NonLatexCitations
 from data_to_paper.latex.latex_to_pdf import check_latex_compilation, remove_figure_envs_from_latex, \
-    replace_special_chars, check_usage_of_unwanted_commands
+    replace_special_chars, check_usage_of_unwanted_commands, check_non_latex_citations
 from data_to_paper.latex.latex_section_tags import get_list_of_tag_pairs_for_section_or_fragment, \
     SECTIONS_OR_FRAGMENTS_TO_TAG_PAIR_OPTIONS
 
@@ -104,11 +104,21 @@ class LatexReviewBackgroundProductsConverser(ReviewBackgroundProductsConverser):
         except LatexProblemInCompilation as e:
             self._raise_self_response_error(str(e))
         self._check_usage_of_unwanted_commands(extracted_section)
+        self._check_usage_of_non_latex_citations(extracted_section)
 
     def _check_usage_of_unwanted_commands(self, extracted_section: str, unwanted_commands: List[str] = None):
         try:
             check_usage_of_unwanted_commands(extracted_section, unwanted_commands)
         except UnwantedCommandsUsedInLatex as e:
+            self._raise_self_response_error(str(e))
+
+    def _check_usage_of_non_latex_citations(self, extracted_section: str):
+        """
+        Check that there are no citations that are not in latex format.
+        """
+        try:
+            check_non_latex_citations(extracted_section)
+        except NonLatexCitations as e:
             self._raise_self_response_error(str(e))
 
     def _check_and_extract_result_from_self_response(self, response: str):
