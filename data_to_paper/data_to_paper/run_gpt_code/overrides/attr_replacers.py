2a8bd8b35699ee9562bd9f25d0581f732d0f5dad
Roy Kishony <rkishony@technion.ac.il>
2023-08-09 18:00:30 +0300

    override all statsmodel fit methods

---
+++ data_to_paper/data_to_paper/run_gpt_code/overrides/attr_replacers.py
@@ -0,0 +1,61 @@
+from typing import Callable
+
+import inspect
+from contextlib import contextmanager
+
+
+def _carefully_get_members(module):
+    try:
+        return inspect.getmembers(module)
+    except Exception:
+        return []
+
+
+def get_all_submodules(module, visited=None):
+    """Recursively get all sub-modules of a module, avoiding loops."""
+    if visited is None:
+        visited = set()
+
+    all_submodules = []
+
+    for name, obj in _carefully_get_members(module):
+        if inspect.ismodule(obj) and obj not in visited:
+            visited.add(obj)
+            all_submodules.append(obj)
+            all_submodules.extend(get_all_submodules(obj, visited))
+
+    return all_submodules
+
+
+@contextmanager
+def method_replacer(module, custom_func_wrapper: Callable, should_replace_func: Callable, recursive: bool = True):
+    """
+    Replace specific methods in a module and all its submodules with a custom wrapper function.
+    `custom_func_wrapper` should be a function that takes the original function as its only argument.
+    `should_replace_func` should be a function that takes the class and attribute name of the function
+    and returns True if the function should be replaced.
+    `recursive` determines whether to replace functions in submodules as well.
+    """
+    originals = {}
+
+    all_modules = [module]
+    if recursive:
+        all_modules += get_all_submodules(module)
+
+    visited_classes = set()
+    for mod in all_modules:
+        for name, obj in _carefully_get_members(mod):
+            if inspect.isclass(obj) and obj not in visited_classes:
+                visited_classes.add(obj)
+                for attr_name, attr_obj in _carefully_get_members(obj):
+                    if attr_name in obj.__dict__ and inspect.isfunction(attr_obj) \
+                            and should_replace_func(obj, attr_name):
+                        original_func = getattr(obj, attr_name)
+                        assert not getattr(original_func, 'is_wrapped', False)
+                        originals[(obj, attr_name)] = original_func
+                        setattr(obj, attr_name, custom_func_wrapper(original_func))
+    try:
+        yield
+    finally:
+        for (obj, attr_name), original_func in originals.items():
+            setattr(obj, attr_name, original_func)
--- data_to_paper/data_to_paper/run_gpt_code/overrides/statsmodels/override_statsmodels_fit.py
+++ data_to_paper/data_to_paper/run_gpt_code/overrides/statsmodels/override_statsmodels_fit.py
@@ -1,44 +1,31 @@
-from contextlib import contextmanager
-from typing import List
+import functools
 
-import pandas as pd
 import statsmodels.api
 
 from .pvalue_dtype import PValueDtype
+from ..attr_replacers import method_replacer
 
-DEFAULT_STAT_CLASSES = ['api.OLS', 'api.WLS', 'api.GLS', 'genmod.generalized_linear_model.GLM']
 
-
-@contextmanager
-def label_pvalues(stat_classes: List[str] = None):
+def label_pvalues():
     """
-    A context manager that patches the pvalues attribute of the given stat classes to be of type PValueDtype.
+    A context manager that replaces the pvalues attribute of all fit functions in statsmodels with a
+    PValueDtype.
     """
-    stat_classes = stat_classes or DEFAULT_STAT_CLASSES
-    original_fits = {}
+    def should_replace_func(obj, attr_name):
+        return attr_name.startswith('fit')
 
-    def custom_fit(self, *args, **kwargs):
-        # Call the original fit method
-        result = original_fits[self.__class__](self, *args, **kwargs)
-        # Patch the pvalues attribute
-        try:
-            if isinstance(result.pvalues, pd.Series):
+    def fit_wrapper(original_func):
+        @functools.wraps(original_func)
+        def wrapped(self, *args, **kwargs):
+            result = original_func(self, *args, **kwargs)
+
+            # Replace the pvalues attribute if it exists
+            try:
                 result.pvalues = result.pvalues.astype(PValueDtype(self.__class__.__name__))
-        except (AttributeError, TypeError, ValueError):
-            pass
-        return result
+            except (AttributeError, TypeError, ValueError):
+                pass
+            return result
+        wrapped.is_wrapped = True
+        return wrapped
 
-    # Backup the original fit methods and replace with custom_fit
-    for stat_class in stat_classes:
-        module = statsmodels
-        for submodule in stat_class.split('.'):
-            module = getattr(module, submodule)
-        cls = module
-        original_fits[cls] = cls.fit
-        cls.fit = custom_fit
-
-    yield
-
-    # Restore the original fit methods
-    for cls, original_fit in original_fits.items():
-        cls.fit = original_fit
+    return method_replacer(statsmodels, fit_wrapper, should_replace_func)
