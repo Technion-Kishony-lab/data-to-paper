8e9177bf544fb5bfb4a02e914988d4157410926f
Roy Kishony <rkishony@technion.ac.il>
2023-09-03 21:51:40 +0300

    diff_strs
    diff

---
+++ tests/functional/utils/test_text_counting.py
@@ -0,0 +1,16 @@
+import pytest
+
+from data_to_paper.utils.text_counting import diff_strs
+
+
+@pytest.mark.parametrize('context, expected', [
+    (0, ' ... (f) [1] [2] [3]  ... '),
+    (1, ' ... e (f) [1] [2] [3] g  ... '),
+    (2, ' ... d e (f) [1] [2] [3] g h  ... '),
+])
+def test_diff_strs(context, expected):
+    str1 = 'a b c d e f g h i j'
+    str2 = 'a b c d e 1 2 3 g h i j'
+
+    assert diff_strs(str1, str2, context=context, add_template='[{}] ', remove_template='({}) ') \
+           == expected
