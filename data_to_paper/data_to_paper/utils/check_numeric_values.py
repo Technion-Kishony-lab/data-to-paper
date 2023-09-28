93b271e0b962db38e80305f96dd05c5cc942c7a4
Roy Kishony <rkishony@technion.ac.il>
2023-09-28 12:28:04 +0300

    allow 1.96 as special numeric (for 95% CI)

--- data_to_paper/data_to_paper/utils/check_numeric_values.py
+++ data_to_paper/data_to_paper/utils/check_numeric_values.py
@@ -202,7 +202,7 @@ def find_non_matching_numeric_values(source: str, target: str, ignore_int_below:
                                      remove_trailing_zeros: bool = False,
                                      ignore_one_with_zeros: bool = True,
                                      ignore_after_smaller_than_sign: bool = True,
-                                     special_numbers_to_ignore: List[str] = ('95', '99', '100'),
+                                     special_numbers_to_ignore: List[str] = ('95', '99', '100', '1.96'),
                                      allow_truncating: bool = True) -> Tuple[List[str], List[str]]:
     """
     Check that all the numerical values mentioned in the target are also mentioned in the source.
@@ -237,7 +237,7 @@ def find_non_matching_numeric_values(source: str, target: str, ignore_int_below:
             continue
 
         # check if the string number is a special number that we want to ignore:
-        if str_target_number in special_numbers_to_ignore:
+        if str_target_number.strip('-').strip('+') in special_numbers_to_ignore:
             continue
 
         num_digits = get_number_of_significant_figures(str_target_number, remove_trailing_zeros)
