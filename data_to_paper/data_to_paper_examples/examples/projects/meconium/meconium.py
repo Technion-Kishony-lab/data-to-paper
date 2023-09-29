96334f8133078788c1422f5b409e487839036741
Roy Kishony <rkishony@technion.ac.il>
2023-09-29 09:19:53 +0300

    nicu dataset - alphanumeric - compressed
    01
    
    db
    
    db

---
+++ data_to_paper/data_to_paper_examples/examples/projects/meconium/meconium.py
@@ -0,0 +1,24 @@
+from data_to_paper_examples.examples.run_project import get_paper
+
+goal = """
+Research goal: 
+Examining the impact of guideline change on neonatal treatment and outcomes.
+
+Hypothesis:
+- Change in treatment policy lead to change in treatments.
+- The change in treatment policy improved neonatal outcome, measured by duration of stay, apgar scores, etc.
+"""
+
+RUN_PARAMETERS = dict(
+    project='meconium',
+    data_filenames=["meconium_nicu_dataset_preprocessed_short.csv"],
+    research_goal=goal,
+    should_do_data_exploration=True,
+)
+
+if __name__ == '__main__':
+    get_paper(**RUN_PARAMETERS,
+              output_folder='paper01',
+              should_mock_servers=True,
+              load_from_repo=True,
+              save_on_repo=True)
