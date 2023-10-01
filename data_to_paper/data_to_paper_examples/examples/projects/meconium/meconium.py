a8308a93b25c3f8a1a1327a03e6fec773c4cfaec
Roy Kishony <rkishony@technion.ac.il>
2023-10-01 11:38:12 +0300

    PAPER 205
    205 n

--- data_to_paper/data_to_paper_examples/examples/projects/meconium/meconium.py
+++ data_to_paper/data_to_paper_examples/examples/projects/meconium/meconium.py
@@ -18,7 +18,7 @@ RUN_PARAMETERS = dict(
 
 if __name__ == '__main__':
     get_paper(**RUN_PARAMETERS,
-              output_folder='paper204',
+              output_folder='paper205',
               should_mock_servers=True,
               load_from_repo=True,
               save_on_repo=True)
