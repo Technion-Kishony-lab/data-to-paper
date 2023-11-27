e0a913a05fe9e5a2ba6cd9b2b43bd529d4c098f3
Roy Kishony <rkishony@technion.ac.il>
2023-11-27 09:58:44 +0200

    run

--- data_to_paper/data_to_paper/researches_types/accuracy_enhance/run.py
+++ data_to_paper/data_to_paper/researches_types/accuracy_enhance/run.py
@@ -1,10 +1,10 @@
-from typing import List, Dict, Tuple
+from typing import List, Dict, Tuple, Callable
 
 import numpy as np
 import matplotlib.pyplot as plt
 
-from data_to_paper.base_steps import PythonDictWithDefinedKeysReviewBackgroundProductsConverser
-from data_to_paper.base_steps.dual_converser import CycleStatus
+from data_to_paper.base_steps import PythonDictWithDefinedKeysReviewBackgroundProductsConverser, \
+    PythonValueReviewBackgroundProductsConverser
 from data_to_paper.conversation.actions_and_conversations import ActionsAndConversations
 from data_to_paper.servers.openai_models import ModelEngine
 from data_to_paper.utils import dedent_triple_quote_str
@@ -20,13 +20,24 @@ def is_prime(number: int) -> bool:
     return True
 
 
-def is_perfect(number: int) -> bool:
-    # return is_prime(number) and number % 10 == 7
+def is_prime_and_sum_digits_divisible_by_7(number: int) -> bool:
+    """a prime number whose sum of digits is divisible by 7 (e.g., 7, 43, 59, 61)"""
     digits = [int(digit) for digit in str(number)]
-    return is_prime(number) and digits[-1] == 7
+    return is_prime(number) and sum(digits) % 7 == 0
 
 
-def check_list_is_ok(numbers: List[int], start: int, end: int) -> Tuple[int, int, int, int]:
+print([number for number in range(100) if is_prime_and_sum_digits_divisible_by_7(number)])
+
+
+def is_prime_and_last_digit_is_7(number: int) -> bool:
+    """a prime number whose last digit is 7 (e.g., 7, 17, 37, 47, 67, 97)"""
+    return is_prime(number) and number % 10 == 7
+
+
+print([number for number in range(100) if is_prime_and_last_digit_is_7(number)])
+
+
+def check_list_is_ok(numbers: List[int], start: int, end: int, is_perfect: Callable) -> Tuple[int, int, int, int]:
     """
     Checks that the given list of numbers is a list of 'perfect' numbers between `start` and `end`.
     Returns the number of:
@@ -56,74 +67,82 @@ def check_list_is_ok(numbers: List[int], start: int, end: int) -> Tuple[int, int
     return number_of_non_perfect, number_of_missing_perfect, number_of_wrong_order, number_of_out_of_range
 
 
-START, END = 100, 200
-TOTAL_ATTEMPTS = 7
-TOTAL_RUNS = 20
-WITH_REVIEW = False
-key_name = 'perfect_numbers'
-perfect_definition = 'prime numbers whose last digit is 7'
-order_name = 'reverse' if START > END else 'correct'
+N_ATTEMPTS = 7
+N_RUNS = 20
+json_key = 'perfect_numbers'
 
-scores = np.zeros([TOTAL_RUNS, TOTAL_ATTEMPTS, 4])
+for START, END in [(100, 200), (100, 400), (200, 100), (400, 100)]:
+    for WITH_LIST in [True, False]:
+        for DELETE_MESSAGES in [True, False]:
+            for is_perfect in [is_prime_and_sum_digits_divisible_by_7, is_prime_and_last_digit_is_7]:
 
-for run in range(TOTAL_RUNS):
-    converser = PythonDictWithDefinedKeysReviewBackgroundProductsConverser(
-        web_conversation_name=None,
-        max_reviewing_rounds=TOTAL_ATTEMPTS if WITH_REVIEW else 0,
-        actions_and_conversations=ActionsAndConversations(),
-        requested_keys=(key_name, ),
-        value_type=Dict[str, List[int]],
-        json_mode=True,
-        model_engine=ModelEngine.GPT4_TURBO,
-        goal_noun=f"a list of all 'perfect numbers' between {START} and {END}",
-        goal_verb='create',
-        system_prompt=f"You are a helpful assistant. We define 'perfect numbers' as {perfect_definition}.",
-        user_initiation_prompt='Create {goal_noun}.\n'
-                               f'Return a JSON object with a single key "{key_name}" mapping to the list.',
-        other_system_prompt='You are a helpful assistant. You will need to check {goal_noun}.',
-        sentence_to_add_at_the_end_of_performer_response=dedent_triple_quote_str("""
-            Please check my list. Return a JSON object with the following keys:
-            "missing": a list of all missing numbers
-            "not_ok": a list of all numbers that are not ok
-            "wrong_order": a list of all numbers that are not in the correct order
-            "out_of_range": a list of all numbers that are out of range
-            
-            The lists should be empty if there are no such mistakes.
-            """),
-    )
-    converser.initialize_dialog()
-    for attempt in range(TOTAL_ATTEMPTS):
-        response, cycle_status = converser.run_one_cycle()
-        result = converser.get_valid_result()
-        numbers = result[key_name]
-        number_of_non_primes, number_of_missing_primes, number_of_wrong_order, number_of_out_of_range = \
-            check_list_is_ok(numbers, START, END)
-        print(f"### Run {run}, attempt {attempt} ### "
-              f"Not ok: {number_of_non_primes}, Missing: {number_of_missing_primes}, "
-              f"Wrong order: {number_of_wrong_order}, Out of range: {number_of_out_of_range}")
-        if not WITH_REVIEW:
-            converser.apply_append_user_message(dedent_triple_quote_str(f"""
-                Check your list:
-                * All numbers should be 'perfect numbers'.
-                * Did you miss any 'perfect numbers'?
-                * Numbers should be in the correct order and in the correct range.
-                If you see problems, please fix the list. 
-                If you think your list is correct, please return the same list again.
-                Return a JSON object with a single key "{key_name}" mapping to the correct list.
-                """))
-        scores[run, attempt, :] = \
-            number_of_non_primes, number_of_missing_primes, number_of_wrong_order, number_of_out_of_range
-        if WITH_REVIEW and cycle_status is not CycleStatus.NOT_APPROVED_BY_OTHER:
-            break
-        if attempt > 0:
-            converser.apply_delete_messages([-3, -4])
-    scores[run, attempt + 1:, :] = scores[run, attempt:attempt + 1, :]
+                print(f'\n\n' + '=' * 100 + f'\n'
+                      f'### RANGE: {START}-{END}, WITH_LIST: {WITH_LIST}, DELETE_MESSAGES: {DELETE_MESSAGES}, '
+                      f'\nIS_PERFECT: "{is_perfect.__doc__}"\n' + '=' * 100 + '\n')
 
-    np.save(f'scores_{TOTAL_RUNS}_runs_{TOTAL_ATTEMPTS}_attempts_primes_{START}_{END}_review_{int(WITH_REVIEW)}.npy', scores)
+                perfect_definition = is_perfect.__doc__
+                order_name = 'descending' if START > END else 'ascending'
+                list_str = f'\nConsider all numbers between {START} and {END}:\n{list(range(START, END + 1))}' \
+                    if WITH_LIST else ''
+
+                scores = np.zeros([N_RUNS, N_ATTEMPTS, 4])
+
+                for run in range(N_RUNS):
+                    converser = PythonDictWithDefinedKeysReviewBackgroundProductsConverser(
+                        web_conversation_name=None,
+                        max_reviewing_rounds=0,
+                        actions_and_conversations=ActionsAndConversations(),
+                        value_type=Dict[str, List[int]],
+                        requested_keys=[json_key],
+                        json_mode=True,
+                        model_engine=ModelEngine.GPT4_TURBO,
+                        goal_noun=f"a list of all perfect numbers between {START} and {END} in {order_name} order",
+                        goal_verb='create',
+                        system_prompt=f'You are a helpful assistant.\n' + list_str + '\n',
+                        user_initiation_prompt=f"We define a 'perfect' number as {perfect_definition}.\n"
+                                               'Create {goal_noun}.\n\n'
+                                               f'Return JSON with a single key "{json_key}" '
+                                               'mapped to your list of perfect numbers.\n\n'
+                    )
+                    converser.initialize_dialog()
+                    for attempt in range(N_ATTEMPTS):
+                        response, cycle_status = converser.run_one_cycle()
+                        results = converser.get_valid_result()
+                        numbers = results[json_key]
+                        number_of_non_primes, number_of_missing_primes, number_of_wrong_order, number_of_out_of_range = \
+                            check_list_is_ok(numbers, START, END, is_perfect)
+                        print(f"### Run {run}, attempt {attempt} ### "
+                              f"Not ok: {number_of_non_primes}, Missing: {number_of_missing_primes}, "
+                              f"Wrong order: {number_of_wrong_order}, Out of range: {number_of_out_of_range}")
+                        converser.apply_append_user_message(dedent_triple_quote_str(f"""
+                            Check your list:
+                            * are all numbers 'perfect'?
+                            * did you possibly forget to include some 'perfect' numbers?
+                            * are all numbers between {START} and {END}?
+                            * is the list in {order_name} order?
+                            
+                            If you see problems, please correct the list. 
+                            Return JSON with a single key "{json_key}" mapped to your list of perfect numbers.
+                            
+                            If you think your list is already correct, please return the same list again.
+                            """))
+                        scores[run, attempt, :] = \
+                            number_of_non_primes, number_of_missing_primes, number_of_wrong_order, number_of_out_of_range
+                        if attempt > 0 and DELETE_MESSAGES:
+                            converser.apply_delete_messages([-3, -4])
+                    scores[run, attempt + 1:, :] = scores[run, attempt:attempt + 1, :]
+
+                np.save(f'set1_"{perfect_definition}"'
+                        f'_{N_RUNS}_runs'
+                        f'_{N_ATTEMPTS}_attempts'
+                        f'_range_{START}_{END}'
+                        f'_delete_{int(DELETE_MESSAGES)}'
+                        f'_list_{int(WITH_LIST)}'
+                        f'.npy', scores)
 
 
 plt.figure(figsize=(10, 5))
-plt.plot(np.arange(TOTAL_ATTEMPTS), np.mean(scores, axis=0))
+plt.plot(np.arange(N_ATTEMPTS), np.mean(scores, axis=0))
 plt.legend(['Non-primes', 'Missing primes', 'Wrong order', 'Out of range'])
 plt.xlabel('Attempt')
 plt.ylabel('Number of errors')
