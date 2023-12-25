9559b2c32ac8cb3ac4b8037a297670a4f1270c73
Roy Kishony <rkishony@technion.ac.il>
2023-12-26 00:16:03 +0200

    refine goals
    g
    
    g

--- data_to_paper/data_to_paper_examples/examples/projects/congress_twitter/congress_twitter.py
+++ data_to_paper/data_to_paper_examples/examples/projects/congress_twitter/congress_twitter.py
@@ -6,20 +6,22 @@ goals = {
         "Hypothesis: The in-degree and out-degree of a member are associated with the size of their State "
         "(as measured by the number of members in each State), even when controlling for Party and Chamber.\n",
 
-    "centrality_inter_state-state_size":
-        "Goal: Understand factors affecting the social network in-degree and out-degree of members. \n"
-        "Hypothesis 1: The in-degree and out-degree of a member are associated with the size of their State "
-        "(as measured by the number of members in each State), even when controlling for Party and Chamber.\n"
-        "Hypothesis 2: The above association holds even when excluding same-state engagements.",
+    "degree_inter_state-state_size":
+        "Goal: Understand factors affecting the social network inter-state in-degree and out-degree of members. \n"
+        "Hypothesis: The in-degree and out-degree of a member are associated with the size of their State "
+        "(as measured by the number of members in each State), even when controlling for Party and Chamber, "
+        "and even when completely excluding interactions between members from the same State.\n",
 
     "connectivity-state+chamber+party":
-        "Goal: Analyse factors affecting the probability of interaction between members. \n"
-        "Hypothesis: The chance that member A engages with member B is associated with: \n"
-        "(a) The size of the State of member A (as measured by the number of members in each state). \n"
-        "(b) The size of the State of member B. \n"
-        "(c) Whether member A and member B are from the same party. \n"
+        "Goal: Analyse factors affecting the probability of interaction between members.\n"
+        "Hypothesis: The chance that member A engages with member B is associated with:\n"
+        "(a) The size of the State of member A (as measured by the number of members in each state).\n"
+        "(b) The size of the State of member B.\n"
+        "(c) Whether member A and member B are from the same party.\n"
         "(d) Whether member A and member B are from the same Chamber.\n"
         "(e) Whether member A and member B are from the same State.",
+
+    'open-ended': None,
 }
 
 
@@ -30,10 +32,10 @@ For example, do NOT choose a goal like "Democrats are more/less engaged than Rep
 """
 
 if __name__ == '__main__':
-    for key, goal in goals.items():
+    for key, goal in list(goals.items())[1:2]:
         for jrun in [0]:
 
-            goal = "Goal and Hypothesis:\n" + goal
+            goal = "Goal and Hypothesis:\n" + goal if goal is not None else None
 
             RUN_PARAMETERS = dict(
                 project='congress_twitter',
