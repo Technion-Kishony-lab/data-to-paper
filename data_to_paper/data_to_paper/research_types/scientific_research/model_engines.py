2917d48cd6dea378f96295309345c6d09d4abd76
Roy Kishony <rkishony@technion.ac.il>
2024-01-29 00:06:05 +0200

    delete obsolete DataAnalysisCodeProductsGPT

--- data_to_paper/data_to_paper/research_types/scientific_research/model_engines.py
+++ data_to_paper/data_to_paper/research_types/scientific_research/model_engines.py
@@ -6,7 +6,6 @@ if TYPE_OF_MODELS == 'closed':
     ModelEngine.DEFAULT = ModelEngine.GPT35_TURBO
     CLASSES_TO_MODEL_ENGINES = {
         "DataExplorationCodeProductsGPT": ModelEngine.GPT4,
-        "DataAnalysisCodeProductsGPT": ModelEngine.GPT4,
         "CreateTablesCodeProductsGPT": ModelEngine.GPT4,
         "GetMostSimilarCitations": ModelEngine.GPT4,
         "IsGoalOK": ModelEngine.GPT4,
@@ -18,7 +17,6 @@ elif TYPE_OF_MODELS == 'open':
     ModelEngine.DEFAULT = ModelEngine.LLAMA_2_70b
     CLASSES_TO_MODEL_ENGINES = {
         "DataExplorationCodeProductsGPT": ModelEngine.GPT4,
-        "DataAnalysisCodeProductsGPT": ModelEngine.CODELLAMA,
         "CreateTablesCodeProductsGPT": ModelEngine.CODELLAMA,
         "GetMostSimilarCitations": ModelEngine.LLAMA_2_70b,
         "IsGoalOK": ModelEngine.LLAMA_2_70b,
