d3580da153f5c2abfa5603f53a632cc45f77fb84
rkishony <rkishony@technion.ac.il>
2024-02-22 16:22:49 +0200

    RERUN 1301

--- data_to_paper/data_to_paper_examples/examples/projects/diabetes/outputs/paper1301/data_to_latex.py
+++ data_to_paper/data_to_paper_examples/examples/projects/diabetes/outputs/paper1301/data_to_latex.py
@@ -1,79 +1,72 @@
 
 # IMPORT
 import pandas as pd
-from typing import Dict, Tuple, Optional
-from my_utils import to_latex_with_note, format_p_value
-
-Mapping = Dict[str, Tuple[Optional[str], Optional[str]]]
-
+from my_utils import to_latex_with_note, is_str_in_df, split_mapping, AbbrToNameDef
+from typing import Optional, Dict, Any, Tuple
 
 # PREPARATION FOR ALL TABLES
-def split_mapping(d: Mapping):
-    abbrs_to_names = {abbr: name for abbr, (name, definition) in d.items() if name is not None}
-    names_to_definitions = {name or abbr: definition for abbr, (name, definition) in d.items() if definition is not None}
-    return abbrs_to_names, names_to_definitions
-
-shared_mapping: Mapping = {
-    'PhysActivity': ('Physical Act.', '0: No activity, 1: Any activity'),
-    'BMI': ('BMI', "Body Mass Index"),
-    'Age': ('Age', 'Age in intervals of 5 years. 1: 18-24 --> 13: 80+ yrs'),
-    'Smoker': ('Smoker', '0: Non-smoker, 1: Smoker'),
-    'HighBP': ('High BP', '0: No high BP, 1: High BP'),
-    'HighChol': ('High Chol', '0: No high chol, 1: High chol'),
-    'Education': ('Ed.', 'Education level. 1: None --> 6: College'),
-    'Income': ('Income', 'Income category. 1: <=10K --> 8: >75K'),
-    'Coef.': ('Coef.', None),
-    'Std.Err.': ('Std.Err.', None),
-    'z': ('Z', 'Standardized test statistic'),
-    'P>|z|': ('P-value', 'Significance level of the Z statistic')
+shared_mapping: AbbrToNameDef = {
+    'Coef.': ('Coeff.', 'Coefficient of the logistic regression model'),
+    'Std.Err.': ('Std. Err.', None),
+    'P>|z|': ('Pval', 'P-value'),
+    '[0.025': ('CI LB', '95% Confidence Interval Lower Bound'),
+    '0.975]': ('CI UB', '95% Confidence Interval Upper Bound'),
+    'Age': (None, '13-level age category'),
+    'BMI': (None, 'Body Mass Index'),
+    'Income': (None, 'Income level, 1 to 8'),
+    'Education': (None, 'Education level, 1 to 6'),
+    'HighBP': (None, 'High Blood Pressure (0=no, 1=yes)'),
+    'HighChol': (None, 'High Cholesterol (0=no, 1=yes)'),
+    'PhysActivity': (None, 'Physical Activity in past 30 days (0=no, 1=yes)'),
+    'z': ('z-stat', None),
 }
 
 # TABLE 0:
 df0 = pd.read_pickle('table_0.pkl')
 
-mapping0 = {'mean': ('Mean', None), 'std': ('Std.Dev', None)}
-abbrs_to_names, legend = split_mapping(mapping0)
-df0.rename(columns=abbrs_to_names, index=abbrs_to_names, inplace=True)
+# RENAME ROWS AND COLUMNS
+mapping0 = dict((k, v) for k, v in shared_mapping.items() if is_str_in_df(df0, k)) 
+abbrs_to_names0, legend0 = split_mapping(mapping0)
+df0 = df0.rename(columns=abbrs_to_names0, index=abbrs_to_names0)
 
+# SAVE AS LATEX:
 to_latex_with_note(
-    df0, 
-    'table_0.tex', 
-    caption="Descriptive statistics of Physical Activity stratified by Diabetes", 
-    label='table:descriptive_statistics_0',
-    legend=legend)
+    df0, 'table_0.tex',
+    caption="Descriptive Statistics of Physical Activity Stratified by Diabetes Presence", 
+    label='table:desc_stats',
+    legend=legend0)
+
 
 # TABLE 1:
 df1 = pd.read_pickle('table_1.pkl')
-abbrs_to_names, legend = split_mapping(shared_mapping)
-df1.rename(columns=abbrs_to_names, index=abbrs_to_names, inplace=True)
 
-# FORMAT VALUES
-df1['P-value'] = df1['P-value'].apply(format_p_value)
+# RENAME ROWS AND COLUMNS
+mapping1 = dict((k, v) for k, v in shared_mapping.items() if is_str_in_df(df1, k)) 
+abbrs_to_names1, legend1 = split_mapping(mapping1)
+df1 = df1.rename(columns=abbrs_to_names1, index=abbrs_to_names1)
 
+# SAVE AS LATEX:
 to_latex_with_note(
-    df1, 
-    'table_1.tex',
-    caption="Association between  physical activity and diabetes prevalence",
-    label='table:activity_and_diabetes_1',
-    legend=legend)
+    df1, 'table_1.tex',
+    caption="Association Between Physical Activity and Diabetes Prevalence", 
+    label='table:assoc_pa_diabetes',
+    legend=legend1)
+
 
 # TABLE 2:
 df2 = pd.read_pickle('table_2.pkl')
 
-mapping2 = dict(shared_mapping.items())
-mapping2.update({
-    'PhysActivity:BMI': ('Activity*BMI', "Interaction term between physical activity and BMI"),
-})
+# RENAME ROWS AND COLUMNS
+mapping2 = dict((k, v) for k, v in shared_mapping.items() if is_str_in_df(df2, k)) 
+mapping2 |= {
+    'PhysActivity:BMI': ('PA * BMI', 'Interaction term, Physical Activity and BMI'),
+}
+abbrs_to_names2, legend2 = split_mapping(mapping2)
+df2 = df2.rename(columns=abbrs_to_names2, index=abbrs_to_names2)
 
-abbrs_to_names, legend = split_mapping(mapping2)
-df2.rename(columns=abbrs_to_names, index=abbrs_to_names, inplace=True)
-
-# FORMAT VALUES
-df2['P-value'] = df2['P-value'].apply(format_p_value)
-
+# SAVE AS LATEX:
 to_latex_with_note(
-    df2, 
-    'table_2.tex',
-    caption="Moderating effect of BMI on the association between physical activity and diabetes",
-    label='table:activity_and_diabetes_2',
-    legend=legend)
+    df2, 'table_2.tex',
+    caption="Moderating Effect of BMI on the Association Between Physical Activity and Diabetes Prevalence", 
+    label='table:mod_effect',
+    legend=legend2)
