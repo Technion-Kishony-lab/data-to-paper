73dc9a32b780db1c1b2f752fc5a7501bffac28d9
Tal Ifargan <talifargan@gmail.com>
2024-08-08 18:02:12 +0300

    run_002 fix

--- projects/COVID_CH/humanintervention1/runs/run_002/data_to_latex.py
+++ projects/COVID_CH/humanintervention1/runs/run_002/data_to_latex.py
@@ -5,85 +5,87 @@ from my_utils import to_latex_with_note, is_str_in_df, split_mapping, AbbrToName
 
 # PREPARATION FOR ALL TABLES
 shared_mapping: AbbrToNameDef = {
-    'coef': (None, 'Regression coefficient'),
-    'p-value': (None, 'p-value of the hypothesis test'),
-    'group[T.I]': ('Infected', 'Group: Infected and not vaccinated'),
-    'group[T.N]': ('None', 'Group: No immunity before study'),
-    'group[T.V]': ('Vaccinated', 'Group: Vaccinated and not infected before study'),
-    'sex[T.male]': ('Male', 'Sex of participant'),
-    'variant[T.omicron]': ('Omicron', 'Omicron variant of SARS-CoV-2'),
-    'I': ('Infected', 'Group: Infected and not vaccinated'),
-    'N': ('None', 'Group: No immunity before study'),
-    'V': ('Vaccinated', 'Group: Vaccinated and not infected before study')
+    'N': ('No Imm.', 'No immunity'),
+    'I': ('Inf. Only', 'Infected, unvaccinated'),
+    'V': ('Vac. Only', 'Twice vaccinated, uninfected'),
+    'H': ('Hyb.', 'Hybrid: Infected & Vaccinated'),
+    'delta': (None, 'Delta Variant'),
+    'omicron': (None, 'Omicron Variant'),
+    'coef': ('Coeff.', None),
+    'p-value': ('P-val', None),
 }
 
 # TABLE 0:
 df0 = pd.read_pickle('table_0.pkl')
 
-# DROP UNNECESSARY COLUMNS
-df0.drop(columns='Standard Deviation', inplace=True) 
-
-# RENAME ROWS/COLUMNS
+# RENAME ROWS AND COLUMNS 
 mapping0 = dict((k, v) for k, v in shared_mapping.items() if is_str_in_df(df0, k)) 
 mapping0 |= {
-    'Average Symptom Number': ('Avg. Symptoms', 'Average number of symptoms'),
-    'Count': ('Cases', 'Total number of cases'),
-    'H': ('Hybrid', 'Group: Infected and vaccinated')
+    'Average Symptom Number': ('Avg. Symp.', 'Average Number of Symptoms'),
+    'Standard Deviation': ('Std. Dev.', 'Standard Deviation of Symptoms'),
+    'Count': ('Count', 'Total number of infected individuals in each group and variant'),
 }
-
 abbrs_to_names0, legend0 = split_mapping(mapping0)
-df0.rename(columns=abbrs_to_names0, index=abbrs_to_names0, inplace=True)
+df0 = df0.rename(columns=abbrs_to_names0, index=abbrs_to_names0)
 
-# SAVE AS LATEX
+# SAVE AS LATEX:
 to_latex_with_note(
     df0, 'table_0.tex',
-    caption='Distribution of Average Symptom Count by Group and Variant',
-    label='table:AvgSymptomCount',
+    caption="Avg. symptom no. by immunity group & variant", 
+    label='table:dist_symp',
+    note="Includes only positive cases.",
     legend=legend0)
 
-
-# TABLE 1
+# TABLE 1:
 df1 = pd.read_pickle('table_1.pkl')
 
-# RENAME ROWS/COLUMNS
+# RENAME ROWS AND COLUMNS 
 mapping1 = dict((k, v) for k, v in shared_mapping.items() if is_str_in_df(df1, k)) 
 mapping1 |= {
-    'age': ('Age', 'Age in Years'),
-    'comorbidity': ('Comorb.', 'Existence of pre-existing comorbidities'),
-    'Intercept': (None, 'The constant term in regression analysis')
+    'Intercept': ('Intercept', None),
+    'age': ('Age', None),
+    'sex[T.male]': ('Sex (M)', 'Male Sex'),
+    'comorbidity': ('Comorb.', '1: Yes, 0: No'),
+    'group[T.I]': ('Inf. Only', 'Infected, unvaccinated'),
+    'group[T.N]': ('No Imm.', 'No immunity'),
+    'group[T.V]': ('Vac. Only', 'Twice vaccinated, uninfected'),
 }
-
 abbrs_to_names1, legend1 = split_mapping(mapping1)
-df1.rename(index=abbrs_to_names1, inplace=True)
+df1 = df1.rename(index=abbrs_to_names1)
 
-# SAVE AS LATEX
+# SAVE AS LATEX:
 to_latex_with_note(
     df1, 'table_1.tex',
-    caption='Regression Analysis: Symptom Count and Group, Age, Sex, Comorbidity',
-    label='table:Regression1',
+    caption="Association between symptom numbers and immunity group", 
+    label='table:symp_grp',
+    note="Adjusted for age, sex, and comorbidities",
     legend=legend1)
 
-
-# TABLE 2
+# TABLE 2:
 df2 = pd.read_pickle('table_2.pkl')
 
-# RENAME ROWS/COLUMNS
+# RENAME ROWS AND COLUMNS 
 mapping2 = dict((k, v) for k, v in shared_mapping.items() if is_str_in_df(df2, k)) 
 mapping2 |= {
-    'group[T.I]:variant[T.omicron]': ('Infected * Omicron', 'Interaction: Infected Unvaccinated and Omicron Variant'),
-    'group[T.N]:variant[T.omicron]': ('None * Omicron', 'Interaction: No Immunity and Omicron Variant'),
-    'group[T.V]:variant[T.omicron]': ('Vaccinated * Omicron', 'Interaction: Vaccinated Unvaccinated and Omicron Variant'),
-    'age': ('Age', 'Age in Years'),
-    'comorbidity': ('Comorb.', 'Existence of pre-existing comorbidities'),
-    'Intercept': (None, 'The constant term in the regression analysis')
+    'Intercept': ('Intercept', None),
+    'age': ('Age', None),
+    'sex[T.male]': ('Sex (M)', 'Male Sex'),
+    'comorbidity': ('Comorb.', '1: Yes, 0: No'),
+    'variant[T.omicron]': ('Var. (O)', 'Omicron Variant'),
+    'group[T.I]:variant[T.omicron]': ('Inf. Only * O', 'Interaction term for Infected Only and Omicron Variant'),
+    'group[T.N]:variant[T.omicron]': ('No Imm. * O', 'Interaction term for No Immunity and Omicron Variant'),
+    'group[T.V]:variant[T.omicron]': ('Vac. Only * O', 'Interaction term for Vaccinated Only and Omicron Variant'),
+    'group[T.I]': ('Inf. Only', 'Infected, unvaccinated'),
+    'group[T.N]': ('No Imm.', 'No immunity'),
+    'group[T.V]': ('Vac. Only', 'Twice vaccinated, uninfected'),
 }
-
 abbrs_to_names2, legend2 = split_mapping(mapping2)
-df2.rename(index=abbrs_to_names2, inplace=True)
+df2 = df2.rename(index=abbrs_to_names2)
 
-# SAVE AS LATEX
+# SAVE AS LATEX:
 to_latex_with_note(
     df2, 'table_2.tex',
-    caption='Regression Analysis: Symptom Count, Group, Variant, Interactions, Age, Sex, Comorbidity',
-    label='table:Regression2',
+    caption="Symptom numbers, immunity group, variant, interaction", 
+    label='table:symp_inter',
+    note="Adjusted for age, sex, and comorbidities",
     legend=legend2)
