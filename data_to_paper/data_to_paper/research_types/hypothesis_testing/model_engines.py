b9c95be89b03c05c447dbafbc38538e7c4d8d42b
rkishony <rkishony@technion.ac.il>
2024-08-26 22:39:58 +0300

    model engine WIP

--- data_to_paper/data_to_paper/research_types/hypothesis_testing/model_engines.py
+++ data_to_paper/data_to_paper/research_types/hypothesis_testing/model_engines.py
@@ -1,33 +1,21 @@
 from data_to_paper.servers.model_engine import ModelEngine
 
-TYPE_OF_MODELS = 'closed'
+CODING_MODEL = ModelEngine.GPT4o
 
-if TYPE_OF_MODELS == 'closed':
-    ModelEngine.DEFAULT = ModelEngine.GPT4o_MINI
-    CLASSES_TO_MODEL_ENGINES = {
-        "DataExplorationCodeProductsGPT": ModelEngine.GPT4o,
-        "BaseCreateTablesCodeProductsGPT": ModelEngine.GPT4o,
-        "GetMostSimilarCitations": ModelEngine.GPT4o,
-        "IsGoalOK": ModelEngine.GPT4o,
-        "NoveltyAssessmentReview": ModelEngine.GPT4o,
-        "GoalReviewGPT": ModelEngine.GPT4o,
-        'SectionWriterReviewBackgroundProductsConverser': ModelEngine.GPT4o,
-        "IntroductionSectionWriterReviewGPT": ModelEngine.GPT4o,
-        "DiscussionSectionWriterReviewGPT": ModelEngine.GPT4o,
-    }
-elif TYPE_OF_MODELS == 'open':
-    ModelEngine.DEFAULT = ModelEngine.LLAMA_2_70b
-    CLASSES_TO_MODEL_ENGINES = {
-        "DataExplorationCodeProductsGPT": ModelEngine.GPT4,
-        "DataAnalysisCodeProductsGPT": ModelEngine.CODELLAMA,
-        "CreateDisplayitemsCodeProductsGPT": ModelEngine.CODELLAMA,
-        "GetMostSimilarCitations": ModelEngine.LLAMA_2_70b,
-        "NoveltyAssessmentReview": ModelEngine.LLAMA_2_70b,
-        "IntroductionSectionWriterReviewGPT": ModelEngine.LLAMA_2_70b,
-        "DiscussionSectionWriterReviewGPT": ModelEngine.LLAMA_2_70b,
-    }
-else:
-    raise ValueError(f'Unknown type of models: {TYPE_OF_MODELS}')
+ModelEngine.DEFAULT = ModelEngine.GPT4o_MINI
+CLASSES_TO_MODEL_ENGINES = {
+    "DataExplorationCodeProductsGPT": CODING_MODEL,
+    "DataAnalysisCodeProductsGPT": CODING_MODEL,
+    "CreateDisplayitemsCodeProductsGPT": CODING_MODEL,
+    "BaseCreateTablesCodeProductsGPT": ModelEngine.GPT4o,
+    "GetMostSimilarCitations": ModelEngine.GPT4o,
+    "IsGoalOK": ModelEngine.GPT4o,
+    "NoveltyAssessmentReview": ModelEngine.GPT4o,
+    "GoalReviewGPT": ModelEngine.GPT4o,
+    'SectionWriterReviewBackgroundProductsConverser': ModelEngine.GPT4o,
+    "IntroductionSectionWriterReviewGPT": ModelEngine.GPT4o,
+    "DiscussionSectionWriterReviewGPT": ModelEngine.GPT4o,
+}
 
 
 def get_model_engine_for_class(class_: type) -> ModelEngine:
