e50b197aaa5a395bf2e79e13a10cb4366e3af57e
rkishony <rkishony@technion.ac.il>
2024-09-24 10:38:34 +0300

    Revert "hotfix: cancel windows timeout (currently not working)"
    This reverts commit d0dd2770ca61a967f8948e0b409fc81f7a10e2ec.

--- src/data_to_paper/run_gpt_code/timeout_context.py
+++ src/data_to_paper/run_gpt_code/timeout_context.py
@@ -14,7 +14,7 @@ def timeout_context(seconds, exception=TimeoutError):
 
     # return different context manager depending on the operating system
     if os.name == 'nt':
-        return TimeoutWindowsContext(seconds=seconds, exception=exception) # TODO: currently not working
+        return TimeoutWindowsContext(seconds=seconds, exception=exception)
     else:
         return TimeoutUnixContext(seconds=seconds, exception=exception)
 
