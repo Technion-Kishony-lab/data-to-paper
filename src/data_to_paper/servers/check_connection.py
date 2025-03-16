126bc6e0730467b396a854c2f1ea3e0e65602b6f
Tal Ifargan <talifargan@gmail.com>
2025-03-16 16:14:14 +0200

    refactor openi to litellm to support many providers

--- src/data_to_paper/servers/check_connection.py
+++ src/data_to_paper/servers/check_connection.py
@@ -1,12 +1,18 @@
+import litellm
 import numpy as np
 
-from data_to_paper.env import CODING_MODEL_ENGINE, JSON_MODEL_ENGINE, WRITING_MODEL_ENGINE
+from data_to_paper.env import (
+    CODING_MODEL_ENGINE,
+    JSON_MODEL_ENGINE,
+    WRITING_MODEL_ENGINE,
+)
+from data_to_paper.servers.server_types import InvalidAPIKeyError
 from data_to_paper.terminate.resource_checking import resource_checking
-from data_to_paper.conversation import Message, Role
 
-from data_to_paper.servers.llm_call import OPENAI_SERVER_CALLER
-from data_to_paper.servers.semantic_scholar import SEMANTIC_SCHOLAR_SERVER_CALLER, \
-    SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER
+from data_to_paper.servers.semantic_scholar import (
+    SEMANTIC_SCHOLAR_SERVER_CALLER,
+    SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER,
+)
 
 models_to_check = {CODING_MODEL_ENGINE, JSON_MODEL_ENGINE, WRITING_MODEL_ENGINE}
 
@@ -21,7 +27,9 @@ EMBEDDING_PAPER = {
 @resource_checking("Checking Semantic Scholar Citation-Server connection")
 def check_semantic_scholar_connection():
     # Calling the server. Will raise ServerErrorException, InvalidAPIKeyError, or MissingAPIKeyError
-    responses = SEMANTIC_SCHOLAR_SERVER_CALLER._get_server_response(query='antibiotics', rows=1)
+    responses = SEMANTIC_SCHOLAR_SERVER_CALLER._get_server_response(
+        query="antibiotics", rows=1
+    )
     if len(responses) != 1:
         raise ConnectionError("Incorrect response from Semantic Scholar server.")
 
@@ -29,19 +37,29 @@ def check_semantic_scholar_connection():
 @resource_checking("Checking Semantic Scholar Embedding-Server connection")
 def check_semantic_scholar_embedding_connection():
     # Calling the server. Will raise ServerErrorException, or MissingAPIKeyError
-    responses = SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER._get_server_response(EMBEDDING_PAPER)
+    responses = SEMANTIC_SCHOLAR_EMBEDDING_SERVER_CALLER._get_server_response(
+        EMBEDDING_PAPER
+    )
     if not isinstance(responses, np.ndarray):
-        raise ConnectionError("Incorrect response from Semantic Scholar Embedding server.")
+        raise ConnectionError(
+            "Incorrect response from Semantic Scholar Embedding server."
+        )
 
 
 @resource_checking("Checking LLM Servers connection")
 def check_llm_servers_connection():
     for model_engine in models_to_check:
-        print(f"Checking LLM Server connection for {model_engine}... ", end='')
+        print(f"Checking LLM Server connection for {model_engine}... ", end="")
         # Calling the server. Will raise ServerErrorException, InvalidAPIKeyError, or MissingAPIKeyError
-        OPENAI_SERVER_CALLER._get_server_response(
-            messages=[Message(role=Role.USER, content='Your favorite color. Answer in one word')],
-            model_engine=model_engine)
+        response = litellm.check_valid_key(
+            model=model_engine.values, api_key=model_engine.api_key
+        )
+        if not response:
+            raise InvalidAPIKeyError(
+                server=model_engine.server_name,
+                response=response,
+                api_key=model_engine.api_key,
+            )
         print("OK")
 
 
